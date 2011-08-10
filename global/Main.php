1ed11e64591a9b6928dfd12b1984bcca8882db89
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2011-08-10 12:51:16 +0000

    * Fix de pregunta del arg en em Main.php que generaba un warning, se setea error_reporting y display_errors a 0

--- global/Main.php
+++ global/Main.php
@@ -7,8 +7,9 @@
 
 // Set error reporting level. See PHP manual: XXVI. Error Handling and Logging Functions
 // Note: E_STRICT  (PHP5 compliance) will cause the PHP4 code base to fail.
-error_reporting(E_ALL -E_NOTICE -E_WARNING);
-ini_set('display_errors',0);
+error_reporting(E_ALL);
+//error_reporting(E_ALL -E_NOTICE -E_WARNING);
+ini_set('display_errors',1);
 
 
 // The application root directory
@@ -17,9 +18,14 @@ $appDir = dirname(__FILE__);
 
 //processing using commandline
 if (!empty($argc)) {
+
+	//if called by commandline don't want any error display o reporting
+	error_reporting(0);
+	ini_set('display_errors',0);
+
 	foreach ($argv as $value) {
-		if ($value != 'Main.php') {
-			$parts = split('=',$value);
+		if (!preg_match('/Main.php/',$value)) {
+			$parts = explode('=',$value);
 			$_REQUEST[$parts[0]] = $parts[1];
 			$_POST[$parts[0]] = $parts[1];
 			$_GET[$parts[0]] = $parts[1];
@@ -31,7 +37,7 @@ else
 	$_ENV['PHPMVC_MODE_CLI'] = false;
 
 // Comment out this define to use the regular (external) phpmvc library
-//define('PHPMVC_PERFORM', "1");
+define('PHPMVC_PERFORM', "1");
 
 // Load the application bootup file
 if(defined('PHPMVC_PERFORM'))
