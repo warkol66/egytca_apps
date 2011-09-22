0e6daee63dcd844fbbe3ebea1dd87ad88e2e146e
dcanuhe <dcanuhe@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2011-09-22 16:34:49 +0000

    - agregada opcion de calidad en la captura de la url

--- updates/mdp/branches/version3.0/WEB-INF/classes/modules/common/classes/WebkitHtmlRenderer.php
+++ updates/mdp/branches/version3.0/WEB-INF/classes/modules/common/classes/WebkitHtmlRenderer.php
@@ -20,7 +20,8 @@ class WebkitHtmlRenderer {
 	 * @param string $image
 	 */
 	function render($url, $image = 'default_image.jpg') {
-		$return_var = shell_exec($this->command . ' ' . $url . ' ' . $image);
+		$quality = 20;
+		$return_var = shell_exec($this->command.' --quality '.$quality.' '.$url.' '.$image);
 		if (!file_exists($image))
 			throw new RenderException("No se pudo capturar la imagen.");
 	}
