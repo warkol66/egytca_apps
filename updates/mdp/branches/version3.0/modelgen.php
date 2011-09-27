33425f0012ef7efa33d9f128916de0284fd66a8b
dcanuhe <dcanuhe@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2011-09-27 18:55:35 +0000

    - mejoras menores

--- updates/mdp/branches/version3.0/modelgen.php
+++ updates/mdp/branches/version3.0/modelgen.php
@@ -6,21 +6,21 @@ ini_set('display_errors',1);
 
 function generate() {
 
-$output = '';
+	$projectHome = shell_exec('echo $PWD');
+	$projectHome = substr($projectHome, 0, -1);
 
-$projectHome = shell_exec('echo $PWD');
-$projectHome = substr($projectHome, 0, -1);
+	$command = './migrate';
 
-$command = './migrate';
+	// Borrar salidas viejas
+	shell_exec('echo estoy vacio > stdout.txt');
+	shell_exec('echo estoy vacio > stderr.txt');
 
-// Borrar salidas viejas
-shell_exec('echo estoy vacio > stdout.txt');
-shell_exec('echo estoy vacio > stderr.txt');
+	shell_exec($command.' > stdout.txt 2> stderr.txt');
+	shell_exec($command.' diff > stdout.txt 2> stderr.txt');
+	//shell_exec($command.' migrate > stdout.txt 2> stderr.txt');
 
-shell_exec($command.' > stdout.txt 2> stderr.txt');
-shell_exec($command.' diff > stdout.txt 2> stderr.txt');
-//shell_exec($command.' migrate > stdout.txt 2> stderr.txt');
-
+	echo '<p>listo</p>';
+	
 }
 
 echo '<p>';
@@ -31,6 +31,7 @@ echo '</form>';
 echo '</p>';
 
 if (isset($_POST["go"]) && $_POST["go"] == 'true') {
+	echo '<p>trabajando...</p>';
 	generate();
 }
 
