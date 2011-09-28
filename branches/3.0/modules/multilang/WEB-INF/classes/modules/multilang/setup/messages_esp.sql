69ccff31bce6240732fbf54ae6d521a0b4bfeed7
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2011-09-28 19:22:23 +0000

    * las funcionalidades multilang pasan al modulo services

--- branches/3.0/modules/multilang/WEB-INF/classes/modules/multilang/setup/messages_esp.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `actionLogs_label` WHERE `action` LIKE 'multilang%' AND `language` = 'esp';
-INSERT INTO `actionLogs_label` (`action`, `label`, `language`, `forward`) VALUES ( 'MultilangLanguagesDoEdit', 'Texto editado exitosamente','esp','success');
-INSERT INTO `actionLogs_label` (`action`, `label`, `language`, `forward`) VALUES ( 'MultilangLanguagesDoEdit', 'Error al guardar el texto','esp','failure');
--- branches/3.0/modules/multilang/WEB-INF/classes/modules/multilang/setup/modulesLabel_eng.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM `modules_label` WHERE `name` = 'multilang' and `language` = 'eng';
-INSERT INTO `modules_label` ( `name` , `label` , `description` , `language` ) VALUES ('multilang', 'Multilanguage', 'Manages multilanguage messages', 'eng');
