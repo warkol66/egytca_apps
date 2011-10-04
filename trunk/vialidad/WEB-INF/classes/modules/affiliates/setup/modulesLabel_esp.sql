c9afdae5c59f435a8d52d5c6cb75d4078ef63fa2
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2011-10-04 13:43:27 +0000

    * Fix, el DBConnection no era el mas reciente, por eso el error de codificacion en el install

--- trunk/vialidad/WEB-INF/classes/modules/affiliates/setup/modulesLabel_esp.sql
+++ trunk/vialidad/WEB-INF/classes/modules/affiliates/setup/modulesLabel_esp.sql
@@ -1,3 +1,3 @@
 DELETE FROM `modules_label` WHERE `name` = 'affiliates' and `language` = 'esp';
 OPTIMIZE TABLE `modules_label`;
-INSERT INTO `modules_label` ( `name` , `label` , `description` , `language` ) VALUES ('affiliates', 'Afiliados', 'Administración de afiliados', 'esp');
+INSERT INTO `modules_label` ( `name` , `label` , `description` , `language` ) VALUES ('affiliates', 'Contratistas', 'Administración de Contratistas', 'esp');
