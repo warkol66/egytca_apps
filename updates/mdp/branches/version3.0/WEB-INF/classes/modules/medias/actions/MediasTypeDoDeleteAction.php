7d83116fd185e848d507833d96375ac8939d2db3
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2011-10-06 20:08:01 +0000

    * Fix para el delete

--- updates/mdp/branches/version3.0/WEB-INF/classes/modules/medias/actions/MediasTypeDoDeleteAction.php
+++ updates/mdp/branches/version3.0/WEB-INF/classes/modules/medias/actions/MediasTypeDoDeleteAction.php
@@ -10,28 +10,22 @@ class MediasTypeDoDeleteAction extends BaseAction {
 
 		BaseAction::execute($mapping, $form, $request, $response);
 
-		//////////
-		// Access the Smarty PlugIn instance
-		// Note the reference "=&"
 		$plugInKey = 'SMARTY_PLUGIN';
 		$smarty =& $this->actionServer->getPlugIn($plugInKey);
 		if($smarty == NULL) {
 			echo 'No PlugIn found matching key: '.$plugInKey."<br>\n";
 		}
 
-		if ($_POST["doHardDelete"]) {
-			if (MediaTypePeer::hardDelete($_POST["id"]))
+		$mediaType = MediaTypePeer::get($_POST["id"]);
+		if (!empty($mediaType)) {
+			if ($_POST["doHardDelete"])
+				MediaTypePeer::disableSoftDelete();
+			if ($mediaType->delete())
 				return $mapping->findForwardConfig('success');
 			else
 				return $mapping->findForwardConfig('failure');
 		}
-		else {
-			if (MediaTypePeer::delete($_POST["id"]))
-				return $mapping->findForwardConfig('success');
-			else
-				return $mapping->findForwardConfig('failure');
-		}
-	
+
 	}
 
 }
