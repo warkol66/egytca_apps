72ce3686c5ecc2e11c5cf5d88710b45e0f3e4359
nico <nico@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2011-10-20 16:12:36 +0000

    Modificaciones en AffiliatesVerifiersListAction, ahora extiende de AffiliatesListAction

--- vialidad/WEB-INF/classes/modules/affiliates/actions/AffiliatesVerifiersListAction.php
+++ vialidad/WEB-INF/classes/modules/affiliates/actions/AffiliatesVerifiersListAction.php
@@ -1,41 +1,13 @@
 <?php
 
-class AffiliatesVerifiersListAction extends BaseAction {
+require_once 'AffiliatesListAction.php';
+
+class AffiliatesVerifiersListAction extends AffiliatesListAction {
 
 	function AffiliatesVerifiersListAction() {
-		;
-	}
-
-	function execute($mapping, $form, &$request, &$response) {
-
-    BaseAction::execute($mapping, $form, $request, $response);
-
-		$plugInKey = 'SMARTY_PLUGIN';
-		$smarty =& $this->actionServer->getPlugIn($plugInKey);
-		if($smarty == NULL) {
-			echo 'No PlugIn found matching key: '.$plugInKey."<br>\n";
-		}
-
-		$module = "Affiliates";
-  	$smarty->assign("module",$module);
-
-  	$smarty->assign("message",$_GET["message"]);
-		
-		$affiliatePeer = new AffiliatePeer;
-		$filters = $_GET["filters"];
-		$this->applyFilters($affiliatePeer, $filters, $smarty);
-
-		$pager = $affiliatePeer->getAllPaginatedFiltered($_GET["page"]);
-		
-		$url = "Main.php?do=affiliatesVerifiersList";
-		foreach ($filters as $key => $value)
-			$url .= "&filters[$key]=$value";
-		$smarty->assign("url",$url);
-
-		$smarty->assign("affiliates",$pager->getResult());
-		$smarty->assign("pager",$pager);
-
-		return $mapping->findForwardConfig('success');
+        parent::__construct();
+        $this->set('listAction', 'affiliatesVerifiersList');
+        $this->set('classKey', AffiliatePeer::CLASSKEY_VERIFIER);
 	}
 
 }
