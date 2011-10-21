879e515e51cdd288dee52138b30a925299eefc44
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2011-10-21 13:16:36 +0000

    * Fix a niveles de ausuarios pro afiliado que no estaba funcionando

--- vialidad/WEB-INF/classes/modules/affiliates/actions/AffiliatesUsersLevelsDoEditAction.php
+++ vialidad/WEB-INF/classes/modules/affiliates/actions/AffiliatesUsersLevelsDoEditAction.php
@@ -1,40 +1,15 @@
 <?php
 
-require_once("BaseAction.php");
-
 class AffiliatesUsersLevelsDoEditAction extends BaseAction {
-
-
-	// ----- Constructor ---------------------------------------------------- //
 
 	function AffiliatesUsersLevelsDoEditAction() {
 		;
 	}
 
-
-	// ----- Public Methods ------------------------------------------------- //
-
-	/**
-	* Process the specified HTTP request, and create the corresponding HTTP
-	* response (or forward to another web component that will create it).
-	* Return an <code>ActionForward</code> instance describing where and how
-	* control should be forwarded, or <code>NULL</code> if the response has
-	* already been completed.
-	*
-	* @param ActionConfig		The ActionConfig (mapping) used to select this instance
-	* @param ActionForm			The optional ActionForm bean for this request (if any)
-	* @param HttpRequestBase	The HTTP request we are processing
-	* @param HttpRequestBase	The HTTP response we are creating
-	* @public
-	* @returns ActionForward
-	*/
 	function execute($mapping, $form, &$request, &$response) {
 
     BaseAction::execute($mapping, $form, $request, $response);
 
-		//////////
-		// Access the Smarty PlugIn instance
-		// Note the reference "=&"
 		$plugInKey = 'SMARTY_PLUGIN';
 		$smarty =& $this->actionServer->getPlugIn($plugInKey);
 		if($smarty == NULL) {
@@ -44,27 +19,28 @@ class AffiliatesUsersLevelsDoEditAction extends BaseAction {
 		$module = "Affiliates";
 		$section = "Levels";
 		
-	    $smarty->assign("module",$module);
-	    $smarty->assign("section",$section);
+    $smarty->assign("module",$module);
+    $smarty->assign("section",$section);
 		
-		$levelParams = $_POST['levelParams'];
+		$levelParams = $_POST["params"];
 		
-		if ( !empty($_POST["id"]) ) {
-			//estoy editando un nivel de usuarios existente
+		if (!empty($_POST["id"])) {
 			$level = AffiliateLevelPeer::get($_POST["id"]);
-		} else {
-			//estoy creando un nuevo nivel de usuarios
-			$level = new AffiliateLevel;
+			$level = Common::setObjectFromParams($level,$levelParams);
+			if ($level->isModified() && !$level->save()) {
+				$smarty->assign("currentLevel", $level);
+				$smarty->assign("message","errorUpdate");
+				return $mapping->findForwardConfig('failure');
+			}
 		}
-		Common::setObjectFromParams($level, $levelParams);
-		if (!$level->save()) {
-			$smarty->assign("currentLevel", $level);
-			$smarty->assign("message","errorUpdate");
-			return $mapping->findForwardConfig('failure');
+		else {
+			if (!AffiliateLevelPeer::create($levelParams["name"])) {
+				$smarty->assign("currentLevel", $level);
+				$smarty->assign("message","errorUpdate");
+				return $mapping->findForwardConfig('failure');
+			}
 		}
-
 		return $mapping->findForwardConfig('success');
 	}
 
 }
-?>
