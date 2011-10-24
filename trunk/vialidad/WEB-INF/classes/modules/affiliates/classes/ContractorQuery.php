ca7f61844f3397880cd2e5f213ab3b2db2c9e2bf
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2011-10-24 16:18:15 +0000

    * Propuesta para usar el nuevo pager a partir de los query classes

--- trunk/vialidad/WEB-INF/classes/modules/affiliates/classes/ContractorQuery.php
+++ trunk/vialidad/WEB-INF/classes/modules/affiliates/classes/ContractorQuery.php
@@ -15,4 +15,74 @@
  */
 class ContractorQuery extends BaseContractorQuery {
 
+		/**
+		 * Permite agregar un filtro personalizado a la Query, que puede ser
+		 * traducido al campo correspondiente.
+		 *
+		 * @param   type $filterName
+		 * @param   type $filterValue
+		 * @return  AffiliateQuery
+		 */
+		public function addFilter($filterName, $filterValue) {
+
+			switch ($filterName) {
+				case 'searchString':
+						$this->filterByName("%$filterValue%", Criteria::LIKE);
+						break;
+
+				default:
+						if (in_array(ucfirst($filterName), AffiliatePeer::getFieldNames(BasePeer::TYPE_PHPNAME)))
+								$this->filterBy(ucfirst($filterName), $filterValue);
+						else { //Log - campo inexistente.
+						}
+						break;
+			}
+			return $this;
+		}
+
+		/**
+		 * Permite agregar un filtro personalizado a la Query, que puede ser
+		 * traducido al campo correspondiente.
+		 *
+		 * @param   type $filterName
+		 * @param   type $filterValue
+		 * @return  AffiliateQuery
+		 */
+		public function addFilters($filters) {
+
+			foreach(array_keys($filters) as $filterKey) {
+				if (isset($filters[$filterKey])) {
+					switch ($filterKey) {
+						case 'searchString':
+								$this->filterByName("%$filters[$filterKey]%", Criteria::LIKE);
+								break;
+						case 'perPage':
+								$this->limit($filters[$filterKey]);
+								break;
+						default:
+								if (in_array(ucfirst($filterName), AffiliatePeer::getFieldNames(BasePeer::TYPE_PHPNAME)))
+										$this->filterBy(ucfirst($filterName), $filterValue);
+								else { //Log - campo inexistente.
+								}
+								break;
+					}
+					return $this;
+				}
+			}
+		}
+
+		/**
+		 * Agrega el limite a la consulta, si existe.
+		 *
+		 * @param   int $limit
+		 * @return  AffiliateQuery
+		 */
+		public function limitIfExists($limit = null) {
+
+				if (!empty($limit)) {
+						$this->limit($limit);
+				}
+				return $this;
+		}
+
 } // ContractorQuery
