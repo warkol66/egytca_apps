3db7714cb2ed8b17bac2d9bfe9b69e567f94aa86
nico <nico@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2011-10-25 13:47:57 +0000

    Modificado SuppliersList para que interactue con ModelCriteria y ModelPager

--- vialidad/WEB-INF/classes/modules/vialidad/actions/VialidadSuppliersListAction.php
+++ vialidad/WEB-INF/classes/modules/vialidad/actions/VialidadSuppliersListAction.php
@@ -23,22 +23,23 @@ class VialidadSuppliersListAction extends BaseAction {
 
 		$smarty->assign("message",$_GET["message"]);
 
-		$supplierPeer = new SupplierPeer;
 		$filters = $_GET["filters"];
-
-		$this->applyFilters($supplierPeer, $filters, $smarty);
-
-		$pager = $supplierPeer->getAllPaginatedFiltered($_GET["page"]);
-
+        $pager = $this->createPager($filters, $_GET["page"], $filters['perPage']);
+		
 		$url = "Main.php?do=vialidadSuppliersList";
 		foreach ($filters as $key => $value)
 			$url .= "&filters[$key]=$value";
 		$smarty->assign("url",$url);
 
-		$smarty->assign("suppliers",$pager->getResult());
+        $smarty->assign("filters",$filters);
+		$smarty->assign("suppliers",$pager->getResults());
 		$smarty->assign("pager",$pager);
 
 		return $mapping->findForwardConfig('success');
 	}
+    
+    protected function getQuery($filters) {
+        return SupplierQuery::create()->addFilters($filters);
+    }
 
 }
