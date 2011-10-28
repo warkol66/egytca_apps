8a23a0f24b75061e3069fa1eaa2653c0e6876c52
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2011-10-28 23:15:07 +0000

    * Actualizacion archivos de nuevas versiones, incluye Main y Prepend usando el perform de phpmvc

--- updates/mer/branches/version3.0/Main.php
+++ updates/mer/branches/version3.0/Main.php
@@ -7,6 +7,7 @@
 
 // Set error reporting level. See PHP manual: XXVI. Error Handling and Logging Functions
 // Note: E_STRICT  (PHP5 compliance) will cause the PHP4 code base to fail.
+//error_reporting(E_ALL);
 error_reporting(E_ALL -E_NOTICE -E_WARNING);
 ini_set('display_errors',1);
 
@@ -17,9 +18,15 @@ $appDir = dirname(__FILE__);
 
 //processing using commandline
 if (!empty($argc)) {
+
+	//if called by commandline don't want any error display o reporting
+	error_reporting(0);
+	ini_set('display_errors',0);
+
 	foreach ($argv as $value) {
-		if ($value != 'Main.php') {
-			$parts = split('=',$value);
+		// Exclude call to dispatcher as param
+		if (!strpos($value,'Main.php')) {
+			$parts = explode('=',$value);
 			$_REQUEST[$parts[0]] = $parts[1];
 			$_POST[$parts[0]] = $parts[1];
 			$_GET[$parts[0]] = $parts[1];
