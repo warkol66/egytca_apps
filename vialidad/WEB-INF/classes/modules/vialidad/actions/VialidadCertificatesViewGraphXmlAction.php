55d0ee5f1996d57b88fc2050f3f1409384439ae2
dcanuhe <dcanuhe@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2011-12-15 14:57:02 +0000

    reimplementados graficos de reportes de obras

--- vialidad/WEB-INF/classes/modules/vialidad/actions/VialidadCertificatesViewGraphXmlAction.php
+++ vialidad/WEB-INF/classes/modules/vialidad/actions/VialidadCertificatesViewGraphXmlAction.php
@@ -1,9 +1,137 @@
 <?php
 
+class Period {
+	private $date;
+	
+	function Period($date, $format  = "d-m-Y") {
+		$dateTime = DateTime::createFromFormat($format, $date);
+		$this->date = strtotime("01-".$dateTime->format("m-Y"));
+	}
+	
+	static function getPeriodsInRange($from, $to, $format = "d-m-Y") {
+		$first = new Period($from, $format);
+		$last = new Period($to, $format);
+		
+		$periods = array();
+		for ($aPeriod = clone $first; $aPeriod->isLessEqualThan($last); $aPeriod = $aPeriod->getNextPeriod())
+			$periods[] = $aPeriod;
+		
+		return $periods;
+	}
+	
+	public function __toString() {
+		return date("m-Y", $this->date);
+	}
+	
+	function getMin($format = "d-m-Y") {
+		return date($format, strtotime("1-".date("m-Y", $this->date)));
+	}
+	
+	function getMax($format = "d-m-Y") {
+		return date($format, strtotime(date("t-m-Y", $this->date)));
+	}
+	
+	function getLimits($format = "d-m-Y") {
+		return array("min" => $this->getMin(), "max" => $this->getMax());
+	}
+	
+	function getNextPeriod() {
+		return new Period(date("d-m-Y", strtotime(date("d-m-Y", $this->date)." + 1 month")));
+	}
+	
+	function getPreviousPeriod(){
+		return new Period(date("d-m-Y", strtotime(date("d-m-Y", $this->date)." - 1 month")));
+	}
+	
+	function isGreaterThan($other) {
+		return ($this->date - $other->date) > 0 ? true : false;
+	}
+	
+	function isGreaterEqualThan($other) {
+		return ($this->date - $other->date) >= 0 ? true : false;
+	}
+	
+	function isLessThan($other) {
+		return ($this->date - $other->date) < 0 ? true : false;
+	}
+	
+	function isLessEqualThan($other) {
+		return ($this->date - $other->date) <= 0 ? true : false;
+	}
+	
+	function isEqualTo($other) {
+		return ($this->date - $other->date) == 0 ? true : false;
+	}
+}
+
+class ConstructionPriceData {
+	
+	private $construction;
+	
+	function ConstructionPriceData($construction) {
+		$this->construction = $construction;
+	}
+	
+	function getConstruction() {
+		return $this->construction;
+	}
+	
+	function getPriceOnPeriod($period) {
+		$certificate = CertificateQuery::create()->useMeasurementRecordQuery()->filterByConstruction($this->construction)
+			->filterByMeasurementdate($period->getLimits("d-m-Y"))->endUse()->findOne();
+		return is_null($certificate) ? 0 : $certificate->getTotalPrice();
+	}
+	
+	function getAccumulatedPriceOnPeriod($period) {
+		$certificates = CertificateQuery::create()->useMeasurementRecordQuery()->filterByConstruction($this->construction)
+			->filterByMeasurementdate($period->getMax("d-m-Y"), Criteria::LESS_EQUAL)->endUse()->find();
+		
+		$price = 0;
+		foreach($certificates as $certificate)
+			$price += $certificate->getTotalPrice();
+		
+		return $price;
+	}
+}
+
 class VialidadCertificatesViewGraphXmlAction extends BaseAction {
 
 	function VialidadCertificatesViewGraphXmlAction() {
 		;
+	}
+	
+	function getLimitPeriods($constructionId) {
+		
+		$min = 
+		
+		$max =
+		
+		$query = CertificateQuery::create()->useMeasurementRecordQuery()
+			->filterByConstructionid($_GET['constructionId']);
+		
+		if (!empty($_GET['date']['min'])) {
+			// ignoro el dia del measurementdate
+			$aux = new Period($_GET['date']['min']);
+			$min = $aux->getMin();
+			$query->filterByMeasurementdate($min, Criteria::GREATER_EQUAL);
+		}
+		if (!empty($_GET['date']['max'])) {
+			// ignoro el dia del measurementdate
+			$aux = new Period($_GET['date']['max']);
+			$max = $aux->getMax();
+			$query->filterByMeasurementdate($max, Criteria::LESS_EQUAL);
+		}
+		
+		$certificates = $query->orderByMeasurementdate(Criteria::ASC)->endUse()->find();
+		
+		if ($certificates->count() <= 0)
+			return null;
+		
+		$limits = array();
+		$limits["min"] = $certificates->getFirst()->getMeasurementRecord()->getMeasurementDate("%d-%m-%Y");
+		$limits["max"] = $certificates->getLast()->getMeasurementRecord()->getMeasurementDate("%d-%m-%Y");
+		
+		return $limits;
 	}
 
 	function execute($mapping, $form, &$request, &$response) {
@@ -22,33 +150,16 @@ class VialidadCertificatesViewGraphXmlAction extends BaseAction {
 		$smarty->assign("section",$section);
 		
 		if (!empty($_GET['constructionId'])) {
-			
-			$certificatesQuery = CertificateQuery::create()->useMeasurementRecordQuery()
-					->filterByConstructionid($_GET['constructionId']);
 
-			if (!empty($_GET['date']['min']) && !empty($_GET['date']['max'])) {
-				$certificatesQuery->filterByMeasurementdate($_GET['date'], Criteria::IN);
-			} else if (!empty($_GET['date']['min'])) {
-				$certificatesQuery->filterByMeasurementdate($_GET['date']['min'], Criteria::GREATER_EQUAL);
-			} else if (!empty($_GET['date']['max'])) {
-				$certificatesQuery->filterByMeasurementdate($_GET['date']['max'], Criteria::LESS_EQUAL);
-			} //else don't filter by date
+			$construction = ConstructionQuery::create()->findOneById($_GET['constructionId']);
 			
-			$certificates = $certificatesQuery->orderByMeasurementdate(Criteria::ASC)->endUse()->find();
+			$limits = $this->getLimitPeriods($_GET['constructionId']);
 			
-			$dates = array();
-			$prices = array();
-			$acummulated = 0;
-			foreach ($certificates as $certificate) {
-				$acummulated += $certificate->getTotalPrice();
-				$certificatesArray[] = $certificate->getTotalPrice();
-				$acumulatedArray[] = $acummulated;
-				array_push($dates, $certificate->getMeasurementRecord()->getMeasurementDate('%m-%Y'));
-			}
-			$smarty->assign('construction', ConstructionQuery::create()->findOneById($_GET['constructionId']));
-			$smarty->assign('dates', $dates);
-			$smarty->assign('certificatesArray', $certificatesArray);
-			$smarty->assign('acumulatedArray', $acumulatedArray);
+			$periods = Period::getPeriodsInRange($limits["min"], $limits["max"]);
+			$constructionPriceData = new ConstructionPriceData($construction);
+			
+			$smarty->assign('constructionPriceData', $constructionPriceData);
+			$smarty->assign('periods', $periods);
 			
 			$this->template->template = 'TemplateAjax.tpl';
 			header ("content-type: text/xml; charset=utf-8");
