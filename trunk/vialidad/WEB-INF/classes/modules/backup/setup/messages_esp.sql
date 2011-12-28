06adf865470d9921b30b2e7b651fcf9a6b313537
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2011-12-28 21:52:09 +0000

    * Actualización archivos de instalación de modulo backup y affiliates

--- trunk/vialidad/WEB-INF/classes/modules/backup/setup/messages_esp.sql
+++ trunk/vialidad/WEB-INF/classes/modules/backup/setup/messages_esp.sql
@@ -4,7 +4,7 @@ INSERT INTO `actionLogs_label` (`action`, `label`, `language`, `forward`) VALUES
 INSERT INTO `actionLogs_label` (`action`, `label`, `language`, `forward`) VALUES ( 'BackupCreate', 'Error al crear respaldo en el servidor','esp','failure');
 INSERT INTO `actionLogs_label` (`action`, `label`, `language`, `forward`) VALUES ( 'BackupRestore', 'Respaldo restaurado desde el servidor con éxito','esp','success');
 INSERT INTO `actionLogs_label` (`action`, `label`, `language`, `forward`) VALUES ( 'BackupRestore', 'Error restaurando respaldo desde el servidor','esp','failure');
-INSERT INTO `actionLogs_label` (`action`, `label`, `language`, `forward`) VALUES ( 'BackupRestoreFromFile', 'Respaldo restaurado desde archivo con éxito','esp','success');
-INSERT INTO `actionLogs_label` (`action`, `label`, `language`, `forward`) VALUES ( 'BackupRestoreFromFile', 'Error restaurando respaldo desde el archivo','esp','failure');
 INSERT INTO `actionLogs_label` (`action`, `label`, `language`, `forward`) VALUES ( 'BackupDoDelete', 'Elimando archivo de respaldo','esp','success');
 INSERT INTO `actionLogs_label` (`action`, `label`, `language`, `forward`) VALUES ( 'BackupDoDelete', 'No se pudo eliminar archivo de respaldo','esp','failure');
+INSERT INTO `actionLogs_label` (`action`, `label`, `language`, `forward`) VALUES ( 'BackupSendByEmail', 'Archivo de respaldo enviado por correo electrónico','esp','success');
+INSERT INTO `actionLogs_label` (`action`, `label`, `language`, `forward`) VALUES ( 'BackupSendByEmail', 'No se pudo enviar el archivo de respaldo','esp','failure');
