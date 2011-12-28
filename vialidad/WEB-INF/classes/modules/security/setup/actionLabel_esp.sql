b217e0f8219f05e9161d73db59504e991df90d25
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2011-12-28 21:52:09 +0000

    * Actualización archivos de instalación de modulo backup y affiliates

--- vialidad/WEB-INF/classes/modules/security/setup/actionLabel_esp.sql
+++ vialidad/WEB-INF/classes/modules/security/setup/actionLabel_esp.sql
@@ -1,4 +1,4 @@
 DELETE FROM `security_actionLabel` WHERE `action` LIKE 'Security%' AND `language` = 'esp';
 OPTIMIZE TABLE `security_actionLabel`;
-INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('SecurityEditPermissions', 'Modificar permisos', 'Modificar permisos', 'esp');
-INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('NoPermission', 'No se tiene permiso sobre la acción solicitada', 'No se tiene permiso sobre la acción solicitada', 'esp');
+INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('SecurityEditPermissions', 'Modificar permisos de usuarios', 'Modifica permisos de usuarios', 'esp');
+INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('SecurityNoPermission', 'Notificación de falta de permisos para la acción solicitada', 'Notifica al usuario que no tiene permisos sobre la acción solicitada', 'esp');
