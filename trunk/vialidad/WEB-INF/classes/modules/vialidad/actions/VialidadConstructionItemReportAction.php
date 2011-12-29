2d9668b0efed7ffa080aa198124bfb79ec98b88c
dcanuhe <dcanuhe@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2011-12-29 16:19:20 +0000

    reportes de parametricas en csv

--- trunk/vialidad/WEB-INF/classes/modules/vialidad/actions/VialidadConstructionItemReportAction.php
+++ trunk/vialidad/WEB-INF/classes/modules/vialidad/actions/VialidadConstructionItemReportAction.php
@@ -24,12 +24,54 @@ class VialidadConstructionItemReportAction extends BaseAction {
 		$supplies = SupplyQuery::create()->find();
 		$items = ConstructionItemQuery::create()->find();
 		
+		/* Cosas para usar el ExcelManagement. No probadas. No pensadas a fondo
+		
+		$dataMatrix = array();
+		foreach ($items as $item) {
+			$newRow = array($item->getName());
+			foreach ($supplies as $supply) {
+				$newRow[] = $item->getProportionForSupply($supply);
+			}
+			$dataMatrix[] = $newRow;
+		}
+		
+		$excelManagment = new ExcelManagement();
+		$excelManagment->setTableHeaders($supplies->toArray('name'));
+		$excelManagment->setData($dataMatrix);
+		$excelManagment->sendToClient();
+		
+		fin cosas para usar el ExcelManagement */
+		
+		// Cosas para crear el csv, y descargarlo.
+		// Si queda esto el Action probablemente deberia tener un Do por algun lado
+		ob_end_clean();
+		header('Content-type: text/csv');
+		header('Content-Disposition: attachment; filename="reporte.csv"');
+		$delimiter = ',';
+		// table header
+		foreach ($supplies as $supply)
+			print $delimiter.$supply->getName();
+		print "\n";
+		// table body
+		foreach ($items as $item) {
+			print $item->getName();
+			foreach ($supplies as $supply)
+				print $delimiter.$item->getProportionForSupply($supply).' %';
+			print "\n";
+		}
+		return;
+		// fin cosas para crear el csv
+		
+		/* Cosas para ver el reporte en un template en el browser
+		
 		$this->template->template = 'TemplatePrint.tpl';
 		
 		$smarty->assign('supplies', $supplies);
 		$smarty->assign('items', $items);
 
 		return $mapping->findForwardConfig("success");
+		
+		fin cosas para ver el reporte */
 	}
 		
 }
