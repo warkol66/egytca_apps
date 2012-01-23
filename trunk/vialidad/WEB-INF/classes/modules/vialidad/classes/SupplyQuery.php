0cc1416b217922c548d7448dab412b260d332188
nico <nico@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-01-23 14:52:49 +0000

    Prototipo de BaseQuery, aplicado a EntityFilter de SupplyQuery, usado en VialidadSupplyAutocompleteListXAction

--- trunk/vialidad/WEB-INF/classes/modules/vialidad/classes/SupplyQuery.php
+++ trunk/vialidad/WEB-INF/classes/modules/vialidad/classes/SupplyQuery.php
@@ -69,6 +69,25 @@ class SupplyQuery extends BaseSupplyQuery {
 		}
 		return $this;
 	}
+    
+    public function EntityFilter($filterValue) {
+        $entityQuery = ucfirst($filterValue['entityType']).'Query';
+        if (!class_exists(ucfirst($filterValue['entityType'])) || !class_exists($entityQuery))
+            break; // nothing to filter
+
+        $entity = $entityQuery::create()->findOneById($filterValue['entityId']);
+
+        $filterByEntity = 'filterBy'.ucfirst($filterValue['entityType']);
+
+        if ($filterValue['getCandidates'])
+            $comparison = Criteria::NOT_IN;
+        else
+            $comparison = Criteria::IN;
+
+        $alreadyRelated = SupplyQuery::create()->select("Id")->$filterByEntity($entity)->find()->toArray();
+        $this->filterById($alreadyRelated, $comparison);
+        return $this;
+    }
 
 	/**
 	 * Agrega multiples filtros a la Query.
