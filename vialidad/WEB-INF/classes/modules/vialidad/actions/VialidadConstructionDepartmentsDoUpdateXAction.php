6b970551277deb9f0d738e919a93920ee0a8a8f8
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-02-14 00:45:04 +0000

    * Fix a schema, se pasa el tipo de constructión a fk * Agrego tipo de construcción y departamentos a edit de Construction TODO: Bug en agregar departamentos via chosen, ancho del campo, sale muy estrecho.

---
+++ vialidad/WEB-INF/classes/modules/vialidad/actions/VialidadConstructionDepartmentsDoUpdateXAction.php
@@ -0,0 +1,81 @@
+<?php
+
+class VialidadConstructionDepartmentsDoUpdateXAction extends BaseAction {
+
+	function VialidadConstructionDepartmentsDoUpdateXAction() {
+		;
+	}
+	
+	function arrayHasDepartment($array, $department) {
+		foreach ($array as $e) {
+			if ($e->getId() == $department->getId())
+				return true;
+		}
+		return false;
+	}
+	
+	function addDepartment($construction, $department) {
+		if (!($construction->hasDepartment($department))) {
+			$construction->addDepartment($department);
+			if (!$construction->save()) {
+				$smarty->assign('message', 'failure');
+			} 
+		}
+	}
+	
+	function removeDepartment($construction, $department) {
+		
+		$construction = ConstructionQuery::create()->findOneById($_POST["constructionId"]);
+		$relation = ConstructionDepartmentQuery::create()->filterByConstruction($construction)->filterByDepartment($department)->findOne();
+		
+		if (!empty($relation))
+			try {
+				$relation->delete();
+			}
+			catch (PropelException $exp) {
+				if (ConfigModule::get("global","showPropelExceptions"))
+					print_r($exp->getMessage());
+			}
+	}
+
+	function execute($mapping, $form, &$request, &$response) {
+
+		BaseAction::execute($mapping, $form, $request, $response);
+
+		$plugInKey = 'SMARTY_PLUGIN';
+		$smarty =& $this->actionServer->getPlugIn($plugInKey);
+		if($smarty == NULL) {
+			echo 'No PlugIn found matching key: '.$plugInKey."<br>\n";
+		}
+
+		$module = "Vialidad";
+
+		if (!empty($_POST["constructionId"])) {
+		
+			$construction = ConstructionQuery::create()->findOneById($_POST["constructionId"]);
+			$departmentsIds = $_POST["departmentsIds"];
+			$selectedDepartments = array();
+			
+			foreach ($departmentsIds as $departmentId) {
+				array_push($selectedDepartments, DepartmentQuery::create()->findOneById($departmentId)); 
+			}
+			$associatedDepartments = $construction->getConstructionDepartments();
+			
+			// Quitar los departments que sobren
+			foreach ($associatedDepartments as $e) {
+				if (!$this->arrayHasDepartment($selectedDepartments, $e))
+					$this->removeDepartment($construction, $e);
+			}
+			
+			// Agregar los departments que falten
+			foreach ($selectedDepartments as $e) {
+				if (!$this->arrayHasDepartment($associatedDepartments, $e))
+					$this->addDepartment($construction, $e);
+			}
+			
+		}
+
+//		return $mapping->findForwardConfig('success');
+die;	}
+
+}
--- vialidad/WEB-INF/classes/modules/vialidad/classes/Construction.php
+++ vialidad/WEB-INF/classes/modules/vialidad/classes/Construction.php
@@ -5,7 +5,7 @@
 /**
  * Skeleton subclass for representing a row from the 'vialidad_construction' table.
  *
- * 
+ *
  *
  * You should add additional methods to this class to meet the
  * application requirements.  This class will only be generated as
@@ -14,23 +14,23 @@
  * @package    propel.generator.vialidad.classes
  */
 class Construction extends BaseConstruction {
-	
+
 	function getPriceOnPeriod($period) {
 		require_once 'Period.php';
 		$certificate = CertificateQuery::create()->useMeasurementRecordQuery()->filterByConstruction($this)
 			->filterByMeasurementdate($period->getLimits("d-m-Y"))->endUse()->findOne();
 		return is_null($certificate) ? 0 : $certificate->getTotalPrice();
 	}
-	
+
 	function getAccumulatedPriceOnPeriod($period) {
 		require_once 'Period.php';
 		$certificates = CertificateQuery::create()->useMeasurementRecordQuery()->filterByConstruction($this)
 			->filterByMeasurementdate($period->getMax("d-m-Y"), Criteria::LESS_EQUAL)->endUse()->find();
-		
+
 		$price = 0;
 		foreach($certificates as $certificate)
 			$price += $certificate->getTotalPrice();
-		
+
 		return $price;
 	}
 
@@ -40,7 +40,7 @@ class Construction extends BaseConstruction {
 	 * @return  object contractor
 	 */
 	public function getContractor() {
-		$contract = $this->getContract(); 
+		$contract = $this->getContract();
 		if (empty($contract))
 			$contract = new Contract();
 		return $contract->getAffiliate();
@@ -54,5 +54,19 @@ class Construction extends BaseConstruction {
 	public function getVerifier() {
 		return $this->getAffiliate();
 	}
+
+	/**
+	 * Incia si esta en un Department
+	 *
+	 * @return bool
+	 */
+	public function hasDepartment($department) {
+		$hasDepartment = ConstructionDepartmentQuery::create()->filterByConstruction($this)->filterByDepartment($department)->count();
+		if ($hasDepartment > 0)
+			return true;
+		else
+			return false;
+	}
+
 
 } // Construction
