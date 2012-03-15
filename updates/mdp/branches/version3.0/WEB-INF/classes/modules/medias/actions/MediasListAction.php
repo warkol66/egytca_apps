ce637e34891c09a140df5ae9cc4c414f5626b6ae
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-03-15 17:29:28 +0000

    * Agrego el getPArameters para obtener valores de un array pasado pro post/get

--- updates/mdp/branches/version3.0/WEB-INF/classes/modules/medias/actions/MediasListAction.php
+++ updates/mdp/branches/version3.0/WEB-INF/classes/modules/medias/actions/MediasListAction.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once 'WEB-INF/classes/BaseQuery.php';
-
 class MediasListAction extends BaseAction {
 
 	function MediasListAction() {
@@ -12,9 +10,6 @@ class MediasListAction extends BaseAction {
 
 		BaseAction::execute($mapping, $form, $request, $response);
 
-		//////////
-		// Access the Smarty PlugIn instance
-		// Note the reference "=&"
 		$plugInKey = 'SMARTY_PLUGIN';
 		$smarty = $this->actionServer->getPlugIn($plugInKey);
 		if($smarty == NULL) {
@@ -32,17 +27,17 @@ class MediasListAction extends BaseAction {
 			$smarty->assign("page",$page);
 		}
 
-		$filters = $_GET['filters'];
-		
+		$filters = $request->getParameterValues("filters");
+
 		if (isset($filters["perPage"]) && $filters["perPage"] > 0)
 			$perPage = $filters["perPage"];
 		else
 			$perPage = Common::getRowsPerPage();
 
 		$pager = BaseQuery::create('Media')
-			->filterByAliasof(null)
-			->createPager($filters, $page, $perPage);
-		
+									->filterByAliasof(null)
+									->createPager($filters, $page, $perPage);
+
 		$smarty->assign("medias",$pager->getResults());
 		$smarty->assign("pager",$pager);
 		$smarty->assign("filters", $filters);
