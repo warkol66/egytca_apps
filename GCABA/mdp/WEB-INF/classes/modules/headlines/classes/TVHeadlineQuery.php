315e90ab0afab693adfaac34a86aaffa6ed41cb4
dcanuhe <dcanuhe@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-08-08 19:18:48 +0000

    herencia y mas campos a schema del headline / refactor del parseo de feeds

---
+++ GCABA/mdp/WEB-INF/classes/modules/headlines/classes/TVHeadlineQuery.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+/**
+ * Skeleton subclass for representing a query for one of the subclasses of the 'headlines_headline' table.
+ *
+ * Headline
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.headlines.classes
+ */
+class TVHeadlineQuery extends BaseTVHeadlineQuery {
+
+} // TVHeadlineQuery
