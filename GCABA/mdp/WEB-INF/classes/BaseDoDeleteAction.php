cf7d955459dedc2d60a662c01b3ba0978546c7bb
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-08-09 11:36:16 +0000

    * Acciones Base * Ociones de configuración

---
+++ GCABA/mdp/WEB-INF/classes/BaseDoDeleteAction.php
@@ -0,0 +1,84 @@
+<?php
+
+class BaseDoDeleteAction extends BaseAction {
+	
+	private $entityClassName;
+	protected $smarty;
+	protected $entity;
+	protected $ajaxTemplate;
+	
+	function __construct($entityClassName) {
+		if (empty($entityClassName))
+			throw new Exception('$entityClassName must be set');
+		$this->entityClassName = $entityClassName;
+		if (substr(get_class($this), -7, 1) != "X")
+			$this->ajaxTemplate = str_replace('Action', '', get_class($this)).'X.tpl';
+	}
+
+	function execute($mapping, $form, &$request, &$response) {
+
+		BaseAction::execute($mapping, $form, $request, $response);
+
+		try {
+			$this->preDelete();
+		} catch (Exception $e) {
+			//Elijo la vista basado en si es o no un pedido por AJAX
+			if ($this->isAjax()) {
+				throw $e; // Buscar una mejor forma de que falle AJAX
+			} else {
+				$this->smarty->assign('message', $e->getMessage());
+				return $mapping->findForwardConfig('failure');
+			}
+		}
+		
+		$id = $request->getParameter("id");
+		if (!empty($id)) {
+			$this->entity = BaseQuery::create($this->entityClassName)->findOneById($id);
+			if (is_null($this->entity)) {
+				//Elijo la vista basado en si es o no un pedido por AJAX
+				if ($this->isAjax()) {
+					throw new Exception(); // Buscar una mejor forma de que falle AJAX
+				} else {
+					$this->smarty->assign('notValidId', 'true');
+					return $mapping->findForwardConfig('success');
+				}
+			}
+			
+		}
+        
+		try {
+			$this->entity->delete();
+            $this->postDelete();
+            return $mapping->findForwardConfig('success');
+		} catch (Exception $e) {
+			if (ConfigModule::get("global","showPropelExceptions")){
+				print_r($e->__toString());
+			}
+		}
+
+        return $mapping->findForwardConfig('failure');
+
+//		try {
+//			$this->entity->delete();
+//		} catch (Exception $e) {
+//			if (ConfigModule::get("global","showPropelExceptions")){
+//				print_r($e->__toString());
+//            }
+//		}
+//	
+//		$this->postDelete();
+//
+//		if ($this->entity->isDeleted())
+//			return $mapping->findForwardConfig('success');
+//		else
+//			return $mapping->findForwardConfig('failure');
+	}
+	
+	protected function preDelete() {
+		// default: do nothing
+	}
+	
+	protected function postDelete() {
+		// default: do nothing
+	}
+}
