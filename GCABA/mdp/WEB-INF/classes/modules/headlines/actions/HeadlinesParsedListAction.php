e83cf15588f6a39ffc05855e88de78c7f663f170
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-08-09 15:34:38 +0000

    * Agrego opción para administrar titulares sin usar campañas. TOD Emprolijar, aplicar filtros para poder administrar lso ttitulares traidos de ejes.

--- GCABA/mdp/WEB-INF/classes/modules/headlines/actions/HeadlinesParsedListAction.php
+++ GCABA/mdp/WEB-INF/classes/modules/headlines/actions/HeadlinesParsedListAction.php
@@ -1,22 +1,25 @@
 <?php
+/**
+ * HeadlinesParsedListAction
+ *
+ * Listado de Titulares parseados
+ *
+ * @package    headlines
+ */
+require_once 'BaseListAction.php';
 
-class HeadlinesParsedListAction extends BaseAction {
-
-	function HeadlinesParsedListAction() {
-		;
+class HeadlinesParsedListAction extends BaseListAction {
+	
+	function __construct() {
+		parent::__construct('HeadlineParsed');
 	}
 
-	function execute($mapping, $form, &$request, &$response) {
-
-		BaseAction::execute($mapping, $form, $request, $response);
-
-		$plugInKey = 'SMARTY_PLUGIN';
-		$smarty = $this->actionServer->getPlugIn($plugInKey);
-		if($smarty == NULL) {
-			echo 'No PlugIn found matching key: '.$plugInKey."<br>\n";
-		}
-
-		$campaignId = $request->getParameter('campaignId');
+	protected function preList() {
+		//Reviso si se solicito desde campaing
+		
+		$params["campaignId"] = $campaignId;
+		$campaignId = $_GET['campaignId'];
+		//$campaignId = $request->getParameter('campaignId');
 		$campaign = CampaignQuery::create()->findOneById($campaignId);
 		if ($campaign) {
 			$headlinesParsed = HeadlineParsedQuery::create()
@@ -24,7 +27,7 @@ class HeadlinesParsedListAction extends BaseAction {
 					->filterByStatus(array('max' => HeadlineParsedQuery::STATUS_PROCESSING))
 					->orderByStatus()
 					->find();
-	
+
 			$smarty->assign('campaign', $campaign);
 			$smarty->assign('campaignId', $campaignId);
 			$smarty->assign('headlinesParsed', $headlinesParsed);
@@ -32,10 +35,22 @@ class HeadlinesParsedListAction extends BaseAction {
 			$contentProviders = ConfigModule::get("headlines","contentProvider");
 			$parseStategies = $contentProviders["strategies_options"];
 			$smarty->assign('parseStategies', $parseStategies);
+
+			$params["campaignId"] = $campaignId;
+			$params["status"] = array('max' => HeadlineParsedQuery::STATUS_PROCESSING);
 		}
-		else
-			$smarty->assign('notValidId',true);
-		return $mapping->findForwardConfig('success');
+		else {
+			$this->smarty->assign('campaign', new Campaign());
+			$_GET['filters']['dateTo'] = date();
+			$_GET['filters']['dateFrom'] = date();
+		}
+		parent::preList();
+		$this->module = "Headlines";
 	}
 
+	protected function postList() {
+		parent::postList();
+		$this->smarty->assign("module", $this->module);
+		$this->smarty->assign("section", "Parsed");
+	}
 }
