bc35ead7beda352c5f8430984879d4d27eb8f7b5
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-08-21 14:12:17 +0000

    * Traigo los BaseXXX de tablero, deben ser los mas actualizados

--- trunk/GCABA/agenda/WEB-INF/classes/BaseListAction.php
+++ trunk/GCABA/agenda/WEB-INF/classes/BaseListAction.php
@@ -3,22 +3,23 @@
 class BaseListAction extends BaseAction {
 	
 	private $entityClassName;
-	private $module;
 	protected $smarty;
-	protected $entity;
 	protected $ajaxTemplate;
+	protected $query;
+	protected $results;
+	protected $pager;
 	
-	function __construct($entityClassName,$module) {
+	function __construct($entityClassName) {
 		if (empty($entityClassName))
 			throw new Exception('$entityClassName must be set');
 		$this->entityClassName = $entityClassName;
-		$this->module = $module;
-		$this->ajaxTemplate = str_replace('Action', '', get_class($this)).'X.tpl';
+		if (substr(get_class($this), -7, 1) != "X")
+			$this->ajaxTemplate = str_replace('Action', '', get_class($this)).'X.tpl';
 	}
 
 	function execute($mapping, $form, &$request, &$response) {
 
-		BaseAction::execute($mapping, $form, $request, $response);
+		parent::execute($mapping, $form, $request, $response);
 
 		$plugInKey = 'SMARTY_PLUGIN';
 		$smarty =& $this->actionServer->getPlugIn($plugInKey);
@@ -26,39 +27,51 @@ class BaseListAction extends BaseAction {
 			echo 'No PlugIn found matching key: '.$plugInKey."<br>\n";
 		}
 		$this->smarty =& $smarty;
-
-		$smarty->assign("module", $this->module);
-
-		$this->preBase();
-
-		$pager = BaseQuery::create($this->entityClassName)->createPager($filters, $page, $perPage);
-
-		$smarty->assign(lcfirst($this->entityClassName) . "Coll", $pager->getResults());
-		$smarty->assign("pager",$pager);
-		$smarty->assign("filters", $filters);
-
-		$url = "Main.php?" . "do=" . lcfirst(str_replace('Action', '', get_class($this)));
-		if (isset($_GET['page']))
-			$url .= '&page=' . $_GET['page'];
-		foreach ($filters as $key => $value)
-			$url .= "&filters[$key]=$value";
-		$smarty->assign("url",$url);
-
-		$smarty->assign("filters", $_GET["filters"]);
-		$smarty->assign("page", $_GET["page"]);
-		$smarty->assign("message", $_GET["message"]);
 		
-		$this->postBase();
+		$this->query = BaseQuery::create($this->entityClassName);
+		$this->preList();
 
-		return $mapping->findForwardConfig('success');
+		if (class_exists($this->entityClassName)) {
+
+			if (!$this->notPaginated) {
+	
+				$smarty->assign("moduleConfig", Common::getModuleConfiguration($this->module));
+	
+				$perPage = Common::getRowsPerPage($this->module);
+				$page = $request->getParameter("page");
+				
+				$this->pager = $this->query->createPager($this->filters, $page, $perPage);
+				$this->results = $this->pager->getResults();
+				
+				$smarty->assign("pager",$this->pager);
+			}
+			else {
+				$this->results = $this->query->addFilters($this->filters)->find();
+			}
+
+			$url = "Main.php?" . "do=" . lcfirst(substr_replace(get_class($this),'', strrpos(get_class($this), 'Action'), 6));
+			foreach ($this->filters as $key => $value)
+				$url .= "&filters[$key]=$value";
+			$smarty->assign("url",$url);
+	
+			$smarty->assign("filters", $this->filters);
+			$smarty->assign("page", $page);
+			$smarty->assign("message", $_GET["message"]);
+			
+			$this->postList();
+			
+			$smarty->assign(lcfirst($this->entityClassName) . "Coll", $this->results);
+	
+			return $mapping->findForwardConfig('success');
+		}
 
 	}
 	
-	protected function preBase() {
-		// default: do nothing
+	protected function preList() {
+		$this->filters = $_GET['filters'];
 	}
 	
-	protected function postBase() {
+	protected function postList() {
 		// default: do nothing
 	}
 }
