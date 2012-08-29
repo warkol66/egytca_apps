46aff496f78b5b7917da90420ec3cf988fff7c32
dcanuhe <dcanuhe@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-08-29 23:39:14 +0000

    arreglo al filtro de fechas de headlinesReports

--- GCABA/mdp/WEB-INF/classes/modules/headlines/actions/HeadlinesReportsAction.php
+++ GCABA/mdp/WEB-INF/classes/modules/headlines/actions/HeadlinesReportsAction.php
@@ -17,37 +17,19 @@ class HeadlinesReportsAction extends BaseListAction {
 		parent::preList();
 		$this->module = "Headlines";
 
-		//Obtencion de filtros
-		if (!empty($_GET["page"])) {
-			$page = $_GET["page"];
-			$this->smarty->assign("page",$page);
-		}
-		if (!empty($_GET['filters']))
-			$filters = $_GET['filters'];
-			
-		if (!empty($_GET['filters']['fromDate']))
-			$fromDate = Common::convertToMysqlDateFormat($_GET['filters']['fromDate']);
-
-		if (!empty($_GET['filters']['toDate']))
-			$toDate = Common::convertToMysqlDateFormat($_GET['filters']['toDate']);
-
 		if (!empty($_GET['filters']['mediaId']))
-			$filters = array_merge_recursive($filters, array('Media' => array('entityFilter' => array(
+			$this->filters['entityFilter'] = array(
 				'entityType' => "Media",
 				'entityId' => $_GET['filters']['mediaId']
-			))));
-
-		if (isset($fromDate) || isset($toDate))
-			$filters['rangePublished'] = array('range' => Common::getPeriodArray($fromDate,$toDate));
+			);
 		
-		if (!isset($filters["perPage"]))
-			$perPage = Common::getRowsPerPage();
-		else
-			$perPage = $filters["perPage"];
-
-			$this->filters = $filters;
-
-
+		if (isset($_GET['filters']['fromDate']) || isset($_GET['filters']['toDate']))
+			$this->filters['rangePublished'] = array('range' => Common::getPeriodArray(
+				$_GET['filters']['fromDate'], $_GET['filters']['toDate']
+			));
+		
+		if (!empty($_GET['filters']["perPage"]))
+			$this->perPage = $filters["perPage"];
 	}
 
 	protected function postList() {
