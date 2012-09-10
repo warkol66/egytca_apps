2fc789b20402a94347cffd942d1efe2a800e38ae
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-09-10 14:51:21 +0000

    * Agrego tags, para manejar palabras clave asociadas a titulares. TODO headlinesTagsList e demás actions para administrarlos. Lso actiosn para agregarlo a headines. * Ajustes a filtros

--- trunk/GCABA/mdp/WEB-INF/classes/modules/headlines/classes/HeadlineParsedQuery.php
+++ trunk/GCABA/mdp/WEB-INF/classes/modules/headlines/classes/HeadlineParsedQuery.php
@@ -32,4 +32,12 @@ class HeadlineParsedQuery extends BaseHeadlineParsedQuery {
         return $this->filterByDatepublished($range);
     }
 
+    public function mediaTypeId($mediaTypeId) {
+        $this->useMediaQuery()
+            ->filterByTypeid($mediaTypeId)
+        ->endUse();
+        
+        return $this;
+    }
+
 } // HeadlineParsedQuery
