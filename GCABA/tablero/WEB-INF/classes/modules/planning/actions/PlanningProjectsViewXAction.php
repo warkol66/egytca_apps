9d830f7e62cfda28b9598eea92c18ba962cc2329
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-09-14 14:44:15 +0000

    * Fix valores posibles de selects

--- GCABA/tablero/WEB-INF/classes/modules/planning/actions/PlanningProjectsViewXAction.php
+++ GCABA/tablero/WEB-INF/classes/modules/planning/actions/PlanningProjectsViewXAction.php
@@ -22,11 +22,25 @@ class PlanningProjectsViewXAction extends BaseEditAction {
 
 		$this->smarty->assign("show", true);
 
+		//Constantes y opciones posibles
 		$this->smarty->assign("startingYear", ConfigModule::get("planning","startingYear"));
 		$this->smarty->assign("endingYear", ConfigModule::get("planning","endingYear"));
+		$this->smarty->assign("ministryPriorities", PlanningProject::getMinistryPriorities());
+		$this->smarty->assign("priorities", PlanningProject::getPriorities());
+
+		//Constantes y opciones posibles para la creación de indicadores
+		$this->smarty->assign("planningIndicator", new PlanningIndicator());
+		$this->smarty->assign("indicatorTypes", PlanningIndicator::getIndicatorTypes());
+		$this->smarty->assign("measureFrecuencyTypes", PlanningIndicator::getMeasureFrecuencyTypes());
+		$this->smarty->assign("expectedResultsTypes", PlanningIndicator::getExpectedResultsTypes());
+		$this->smarty->assign("goalTypes", PlanningIndicator::getGoalTypes());
+		$this->smarty->assign("trendTypes", PlanningIndicator::getTrendTypes());
+
 		if ($_GET["showGantt"]) {
 			$this->smarty->assign("showGantt",true);
 			$this->template->template = "TemplateAjax.tpl";
 		}
+
 	}
+
 }
