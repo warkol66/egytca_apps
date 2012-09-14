8c668e389123655e598917a6ba33113ea8086657
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-09-14 14:44:15 +0000

    * Fix valores posibles de selects

--- trunk/GCABA/tablero/WEB-INF/classes/modules/planning/actions/PlanningProjectsShowHistoryXAction.php
+++ trunk/GCABA/tablero/WEB-INF/classes/modules/planning/actions/PlanningProjectsShowHistoryXAction.php
@@ -26,6 +26,17 @@ class PlanningProjectsShowHistoryXAction extends BaseEditAction {
 		$this->smarty->assign("showLog", true);
 		$this->smarty->assign("startingYear", ConfigModule::get("planning","startingYear"));
 		$this->smarty->assign("endingYear", ConfigModule::get("planning","endingYear"));
+		$this->smarty->assign("ministryPriorities", PlanningProject::getMinistryPriorities());
+		$this->smarty->assign("priorities", PlanningProject::getPriorities());
+
+		//Constantes y opciones posibles para la creación de indicadores
+		$this->smarty->assign("planningIndicator", new PlanningIndicator());
+		$this->smarty->assign("indicatorTypes", PlanningIndicator::getIndicatorTypes());
+		$this->smarty->assign("measureFrecuencyTypes", PlanningIndicator::getMeasureFrecuencyTypes());
+		$this->smarty->assign("expectedResultsTypes", PlanningIndicator::getExpectedResultsTypes());
+		$this->smarty->assign("goalTypes", PlanningIndicator::getGoalTypes());
+		$this->smarty->assign("trendTypes", PlanningIndicator::getTrendTypes());
+
 	}
     
 }
