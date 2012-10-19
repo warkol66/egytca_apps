f2cfba062fbd7c30f860be51109c1dce62a2c4e5
dcanuhe <dcanuhe@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-10-19 02:32:28 +0000

    HeadlineImageDoCreateResampledCopy: capturar exceptions si se corre desde el browser

--- trunk/GCABA/mdp/WEB-INF/classes/modules/headlines/actions/HeadlinesImageDoCreateResampledCopyAction.php
+++ trunk/GCABA/mdp/WEB-INF/classes/modules/headlines/actions/HeadlinesImageDoCreateResampledCopyAction.php
@@ -8,11 +8,32 @@ class HeadlinesImageDoCreateResampledCopyAction extends BaseAction {
 
 		parent::execute($mapping, $form, $request, $response);
 		
+		$plugInKey = 'SMARTY_PLUGIN';
+		$smarty = $this->actionServer->getPlugIn($plugInKey);
+		if($smarty == NULL) {
+			echo 'No PlugIn found matching key: '.$plugInKey."<br>\n";
+		}
+		
 		if (!empty($_REQUEST['id'])) {
 			$attachment = HeadlineAttachmentQuery::create()->findOneById($_REQUEST['id']);
 			$input = $attachment->getRealpath();
 			$output = $attachment->getSecondaryDataRealpath();
-			HeadlineImageResampler::copyResampled($input, $output);
+			try {
+				HeadlineImageResampler::copyResampled($input, $output);
+			} catch (Exception $e) {
+				if ($_ENV['PHPMVC_MODE_CLI']) {
+					throw $e;
+				} else {
+					$smarty->assign("errorMessage", $e->getMessage());
+					return $mapping->findForwardConfig("success");
+				}
+			}
+			
+			return $mapping->findForwardConfig("success");
+			
+		} else {
+			$smarty->assign("errorMessage", "ID paramater not found");
+			return $mapping->findForwardConfig("success");
 		}
 	}
 }
