b131e310d291cc7ccadc04343b3f0c951181f94f
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-10-29 20:31:13 +0000

    *modificacion en la forma de agregar clientes y desarrollos a los requerimientos

--- trunk/egytca/support/WEB-INF/classes/modules/requirements/actions/RequirementsDoEditAction.php
+++ trunk/egytca/support/WEB-INF/classes/modules/requirements/actions/RequirementsDoEditAction.php
@@ -12,5 +12,20 @@ class RequirementsDoEditAction extends BaseDoEditAction {
 	function __construct() {
 		parent::__construct('Requirement');
 	}
+	
+	function postUpdate(){
+		parent::postUpdate();
+		
+		if(!empty($_POST["params"]["developmentId"])){
+			$development = DevelopmentQuery::create()->findOneById($_POST["params"]["developmentId"]);
+			$client = $development->getClientid();
+			
+			if(!empty($client)){
+				$this->entity->setClientid($client)->save();
+			}else{
+				$this->entity->setClientid(null)->save();
+			}
+		}
+	}
 
 }
