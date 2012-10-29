4ad276e073905db8e7836f85e6a6a2aecf1fea57
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-10-29 05:56:10 +0000

    agregar clientes a desarrollos y desarrollos y clientes a requerimientos

--- trunk/egytca/support/WEB-INF/classes/modules/requirements/actions/RequirementsDevelopmentsEditAction.php
+++ trunk/egytca/support/WEB-INF/classes/modules/requirements/actions/RequirementsDevelopmentsEditAction.php
@@ -10,74 +10,21 @@
 
 class RequirementsDevelopmentsEditAction extends BaseEditAction {
 	
-	/*function __construct() {
+	function __construct() {
 		parent::__construct('Development');
 	}
 	
 	protected function postEdit() {
 		parent::postEdit();
-	}*/
-	
-	function RequirementsDevelopmentsEditAction() {
-		;
-	}
-
-	function execute($mapping, $form, &$request, &$response) {
-
-		BaseAction::execute($mapping, $form, $request, $response);
-
-		$plugInKey = 'SMARTY_PLUGIN';
-		$smarty =& $this->actionServer->getPlugIn($plugInKey);
-		if($smarty == NULL) {
-			echo 'No PlugIn found matching key: '.$plugInKey."<br>\n";
-		}
-
-		$module = "Requirements";
-		$smarty->assign("module",$module);
-		$section = "Developments";
-		$smarty->assign("section",$section);
-
-		$filters = $_GET["filters"];
-		$smarty->assign("filters",$filters);
-
-		$id = $request->getParameter("id");
-		$smarty->assign("id",$id);
 		
-		//caso: editar development
-		if (!empty($id)) {
-			
-			$development = DevelopmentQuery::create()->findOneById($id);
-			
-			//caso: no existe development con ese id
-			if (empty($development)){
-				
-				$smarty->assign("notValidId","true");
-				$smarty->assign("action","create");
-			
-			}
-			//caso: existe el development
-			else{
-				
-				$smarty->assign("action","edit");
-				
-				$attendants = UserQuery::create()
-					->orderByName()
-					->findByActive(1);
-					
-				$smarty->assign("attendants",$attendants);					
-			}
-		}
-		//caso: crear nuevo development
-		else{
-			
-			$development = new Development();
-			$smarty->assign("action","create");
-			
-		}
-
-		$smarty->assign("development",$development);
-
-		return $mapping->findForwardConfig('success');
+		$module = "Requirements";
+		$this->smarty->assign("module",$module);
+		$section = "Developments";
+		$this->smarty->assign("section",$section);
+		
+		//clientes, desarrollos y recursos
+		$this->smarty->assign("affiliates", AffiliateQuery::create()->find());
+		$this->smarty->assign("attendants",UserQuery::create()->orderByName()->findByActive(1));	
 	}
 
 }
