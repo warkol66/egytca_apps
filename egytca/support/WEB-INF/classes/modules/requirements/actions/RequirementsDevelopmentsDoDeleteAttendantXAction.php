712e2b6bc9f548d1463f0df18c1f7d7bea5bc707
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-11-01 19:29:48 +0000

    *cambios en la forma de desasociar requerimientos

--- egytca/support/WEB-INF/classes/modules/requirements/actions/RequirementsDevelopmentsDoDeleteAttendantXAction.php
+++ egytca/support/WEB-INF/classes/modules/requirements/actions/RequirementsDevelopmentsDoDeleteAttendantXAction.php
@@ -5,11 +5,53 @@
  * Elimina Recurso (Attendant)
  *
  */
-
+/*
 class RequirementsDevelopmentsDoDeleteAttendantXAction extends BaseDoDeleteAction {
 
 	function __construct() {
 		parent::__construct('Attendant');
 	}
 	
+}*/
+
+class RequirementsDevelopmentsDoDeleteAttendantXAction extends BaseAction{
+
+	 function execute($mapping, $form, &$request, &$response) {
+		BaseAction::execute($mapping, $form, $request, $response);
+		
+		$plugInKey = 'SMARTY_PLUGIN';
+				$smarty =& $this->actionServer->getPlugIn($plugInKey);
+				if($smarty == NULL) {
+					echo 'No PlugIn found matching key: '.$plugInKey."<br>\n";
+				}
+				
+				$id = $request->getParameter("id");
+				if (!empty($id)) {
+					$attendant = BaseQuery::create('Attendant')->findOneById($id);
+					$user = $attendant->getUser();
+					if (is_null($attendant)) {
+						//Elijo la vista basado en si es o no un pedido por AJAX
+						if ($this->isAjax()) {
+							throw new Exception(); // Buscar una mejor forma de que falle AJAX
+						} else {
+							$smarty->assign('notValidId', 'true');
+							return $this->addParamsAndFiltersToForwards($params, $filters, $mapping,'success');
+						}
+					}
+					
+				}
+				
+				try {
+					$attendant->delete();
+						//return $this->addParamsAndFiltersToForwards($params, $filters, $mapping,'success');
+				} catch (Exception $e) {
+					if (ConfigModule::get("global","showPropelExceptions")){
+						print_r($e->__toString());
+					}
+				}
+				
+				$smarty->assign("message",$_POST["message"]);
+				$smarty->assign("attendant",$user);
+				return $mapping->findForwardConfig('success');
+		}
 }
