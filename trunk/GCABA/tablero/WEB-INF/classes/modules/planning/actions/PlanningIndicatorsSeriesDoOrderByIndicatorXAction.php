ef48c7229d0ead1e1f9b79ec892e41618b01f1d3
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-11-15 14:21:12 +0000

    * Fixes a Indicadores * Agrego estilo para listado li

--- trunk/GCABA/tablero/WEB-INF/classes/modules/planning/actions/PlanningIndicatorsSeriesDoOrderByIndicatorXAction.php
+++ trunk/GCABA/tablero/WEB-INF/classes/modules/planning/actions/PlanningIndicatorsSeriesDoOrderByIndicatorXAction.php
@@ -1,32 +1,22 @@
 <?php
 /**
 * IndicatorsSeriesDoOrderByIndicatorXAction
-* 
+*
 * Permite mediante Ajax el cambio de orden de los proyectos disponibles
-* 
-* @package  projects
+*
+* @package  indicators
 */
-
-require_once("BaseAction.php");
 
 class PlanningIndicatorsSeriesDoOrderByIndicatorXAction extends BaseAction {
 
 	function PlanningIndicatorsSeriesDoOrderByIndicatorXAction() {
-
+		;
 	}
 
 	function execute($mapping, $form, &$request, &$response) {
 
-    BaseAction::execute($mapping, $form, $request, $response);
+		BaseAction::execute($mapping, $form, $request, $response);
 
-    /**
-    * Use a different template
-    */
-		$this->template->template = "TemplateAjax.tpl";
-
-		//////////
-		// Access the Smarty PlugIn instance
-		// Note the reference "=&"
 		$plugInKey = 'SMARTY_PLUGIN';
 		$smarty =& $this->actionServer->getPlugIn($plugInKey);
 		if($smarty == NULL) {
@@ -34,18 +24,16 @@ class PlanningIndicatorsSeriesDoOrderByIndicatorXAction extends BaseAction {
 		}
 
 		$module = "Indicators";
-
 		parse_str($_POST['data']);
-		
+
 		for ($i = 0; $i < count($seriesList); $i++) {
 			if($seriesList[$i] != NULL) {
-                $serie=PlanningIndicatorSerieQuery::create()->findPk($seriesList[$i]);
-                $serie->setOrder($i);
-                $serie->save();
-                echo $serie->getId();
+				$serie=PlanningIndicatorSerieQuery::create()->findPk($seriesList[$i]);
+				$serie->setOrder($i);
+				$serie->save();
+				echo $serie->getId();
 			}
-   		}
-
+		}
 		return $mapping->findForwardConfig('success');
 
 	}
