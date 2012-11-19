1523e033e70d2db9211915cce290c51dea2945fa
dcanuhe <dcanuhe@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-11-19 21:12:10 +0000

    bug fix orderBy nonexisting field

--- trunk/GCABA/tablero/WEB-INF/classes/modules/planning/classes/OperativeObjectiveQuery.php
+++ trunk/GCABA/tablero/WEB-INF/classes/modules/planning/classes/OperativeObjectiveQuery.php
@@ -21,7 +21,7 @@ class OperativeObjectiveQuery extends BaseOperativeObjectiveQuery {
 		$this->leftJoinMinistryObjective()
 			->leftJoinPosition('Position')
 			->addCond('cond1', PositionPeer::VERSIONID, PositionVersionQuery::getLastVersionId(), Criteria::EQUAL)
-			->orderBy(PlanningProjectPeer::INTERNALCODE)
+			->orderByInternalCode()
 		;
 	}
 
