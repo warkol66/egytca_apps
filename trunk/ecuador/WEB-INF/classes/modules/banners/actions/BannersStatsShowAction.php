d616a4166217604f2993f42857dd621418434420
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-12-20 20:44:32 +0000

    *Refactor modulo banners

--- trunk/ecuador/WEB-INF/classes/modules/banners/actions/BannersStatsShowAction.php
+++ trunk/ecuador/WEB-INF/classes/modules/banners/actions/BannersStatsShowAction.php
@@ -5,9 +5,25 @@
  * Muestra las estadísticas de los banners
  * @package banners
  */
-class BannersStatsShowAction extends BaseAction {
+class BannersStatsShowAction extends BaseListAction {
+	
+	function __construct() {
+		parent::__construct('Banner');
+	}
+	
+	protected function postList(){
+		parent::postList();
+		
+		$this->smarty->assign("module","Banners");
+		$this->smarty->assign("section","Stats");
+		
+		$this->smarty->assign("zones", BannerZoneQuery::create()->find());
+		$this->smarty->assign("banners", BannerQuery::create()->find());
+		$this->smarty->assign("clients", BannerClientQuery::create()->find());
+		
+	}
 
-	function BannersStatsShowAction() {
+	/*function BannersStatsShowAction() {
 		;
 	}
 
@@ -28,12 +44,9 @@ class BannersStatsShowAction extends BaseAction {
 		$section = "Stats";
 		$smarty->assign("section",$section);
 
-		$zones = BannerZonePeer::getAll();
-		$smarty->assign("zones", $zones);
-		$banners = BannerPeer::getAll();
-		$smarty->assign("banners", $banners);
-		$clients = BannerClientPeer::getAll();
-		$smarty->assign("clients", $clients);
+		$smarty->assign("zones", BannerZoneQuery::create()->find());
+		$smarty->assign("banners", BannerQuery::create()->find());
+		$smarty->assign("clients", BannerClientQuery::create()->find());
 
 
 		//Llamada con parámetros
@@ -63,6 +76,6 @@ class BannersStatsShowAction extends BaseAction {
 		$smarty->assign("stats", $stats);
 
 		return $mapping->findForwardConfig('success');
-	}
+	}*/
 
 }
