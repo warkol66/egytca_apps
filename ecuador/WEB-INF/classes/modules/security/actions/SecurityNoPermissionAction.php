f1ff3fe76c446f8ec2b720889542f44967c66128
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-12-27 14:59:40 +0000

    * Actualizo modulo security con fixes de edición de permisos

--- ecuador/WEB-INF/classes/modules/security/actions/SecurityNoPermissionAction.php
+++ ecuador/WEB-INF/classes/modules/security/actions/SecurityNoPermissionAction.php
@@ -8,21 +8,21 @@ class SecurityNoPermissionAction extends BaseAction {
 
 	function execute($mapping, $form, &$request, &$response) {
 
-		BaseAction::execute($mapping, $form, $request, $response);
+    BaseAction::execute($mapping, $form, $request, $response);
 
 		$plugInKey = 'SMARTY_PLUGIN';
 		$smarty =& $this->actionServer->getPlugIn($plugInKey);
 		if($smarty == NULL) {
 			echo 'No PlugIn found matching key: '.$plugInKey."<br>\n";
 		}
-
+		
 		$module = "Security";
 		$section = "No Permission";
 
 		$smarty->assign("module",$module);
 		$smarty->assign("section",$section);
 
-		if (empty($_SESSION["loginUser"]) && empty($_SESSION["loginAffiliateUser"]) && empty($_SESSION["loginClientUser"]))
+		if (empty($_SESSION["loginUser"]) && empty($_SESSION["loginAffiliateUser"]))
 			$this->template->template = "TemplatePublic.tpl";
 
 		return $mapping->findForwardConfig('success');
