4adc9c3bf144484eaf34f4d09caf739b4362ead8
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2012-12-28 17:24:27 +0000

    * Fix al upload (Copiado del upload de templates GCABA/mdp)

--- ecuador/WEB-INF/classes/modules/banners/actions/BannersDoEditAction.php
+++ ecuador/WEB-INF/classes/modules/banners/actions/BannersDoEditAction.php
@@ -6,44 +6,37 @@
  * @package banners
  */
 class BannersDoEditAction extends BaseDoEditAction {
-	
+
 	function __construct() {
 		parent::__construct('Banner');
 	}
-	
-	protected function preUpdate(){
+
+	protected function preUpdate() {
 		parent::preUpdate();
-		
-		if (!$_FILES["document_file"]['name'] == ''){
+
+		if (!$_FILES["document_file"]['name'] == '') {
 			$path = pathinfo($_FILES["document_file"]['name']);
 			$this->entityParams['extension'] = strtolower($path['extension']);
-			
 		}
-		
 	}
-	
+
 	protected function postUpdate(){
 		parent::postUpdate();
-		
-		$this->smarty->assign("module","Banners");
 
+		$this->smarty->assign("module","Banners");
 		$this->entity->removeFromAllZones();
-		
 		$zones = $_POST['params']['zones'];
-		foreach($zones as $zoneId){
+
+		foreach($zones as $zoneId)
 			$this->entity->addToZone($zoneId, $this->entity->getId());
-		}
-		
+
 		global $appDir;
 		$bannersPath = realpath($appDir . '/WEB-INF/classes/modules/banners/files/');
-		//arreglar upload
-		if(is_uploaded_file($_FILES["document_file"]['tmp_name'])){
-			move_uploaded_file($_FILES["document_file"]['tmp_name'], $bannersPath . $this->entity->getId());/*
-				$this->smarty->assign("message", "uploadFailure");
-				die();
-			}*/
-		}
-		
+
+		if (!empty($_FILES["document_file"]['tmp_name'])
+					&& !move_uploaded_file($_FILES["document_file"]['tmp_name'], $bannersPath . "/" . $this->entity->getId()))
+			$this->params["uploadeFailure"] = true;
+
 	}
 
 }
