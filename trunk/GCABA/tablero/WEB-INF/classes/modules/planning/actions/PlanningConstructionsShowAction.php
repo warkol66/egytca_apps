08515a71ac12eaf78b4ae5c7d490a60aecf0c538
dcanuhe <dcanuhe@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-03-07 06:54:55 +0000

    planningConstructionsShow y nuevo link de semaforo de constructions en panelResultsShow

---
+++ trunk/GCABA/tablero/WEB-INF/classes/modules/planning/actions/PlanningConstructionsShowAction.php
@@ -0,0 +1,40 @@
+<?php
+
+class PlanningConstructionsShowAction extends BaseAction {
+	
+	function execute($mapping, $form, &$request, &$response) {
+
+		parent::execute($mapping, $form, $request, $response);
+		
+		$plugInKey = 'SMARTY_PLUGIN';
+		$smarty =& $this->actionServer->getPlugIn($plugInKey);
+		if($smarty == NULL) {
+			echo 'No PlugIn found matching key: '.$plugInKey."<br>\n";
+		}
+
+		$module = "Planning";
+		$section = "Constructions";
+
+		$smarty->assign("module",$module);
+		$smarty->assign("section",$section);
+
+		$moduleConfig = Common::getModuleConfiguration($module);
+		$smarty->assign("moduleConfig",$moduleConfig);
+
+		if (!empty($_GET['positionId'])) {
+			$filters = $_GET['filters'];
+			$constructionsQuery = BaseQuery::create('PlanningConstruction')->addFilters($filters);
+			$position = PositionQuery::create()->findOneById($_GET['positionId']);
+			$smarty->assign("position", $position);
+			$constructions = empty($_GET['color']) ?
+				$position->getAllConstructionsWithDescendants($constructionsQuery) : $position->getConstructionsByStatusColor($_GET['color'],$constructionsQuery);
+		}
+
+		$smarty->assign("constructions", $constructions);
+
+		$smarty->assign("filters", $filters);
+
+		return $mapping->findForwardConfig('success');
+	}
+
+}
