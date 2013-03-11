ff933289123731317d71c06442a923976ea300a8
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-03-11 16:49:48 +0000

    *Eliminacion de obras cuando se borran proyectos

--- GCABA/tablero/WEB-INF/classes/modules/planning/actions/PlanningProjectsDoDeleteAction.php
+++ GCABA/tablero/WEB-INF/classes/modules/planning/actions/PlanningProjectsDoDeleteAction.php
@@ -16,8 +16,15 @@ class PlanningProjectsDoDeleteAction extends BaseDoDeleteAction {
 	}
 	
 	function postDelete(){
-        parent::postDelete();
-       
-        $activities = PlanningActivityQuery::create()->filterByObjectType('Project')->filterByObjectid($_POST['id'])->delete();
+		parent::postDelete();
+
+		//elimino actividades y obras asociadas
+		//el delete hace un archive antes de eliminar el objeto
+		PlanningActivityQuery::create()->filterByObjectType('Project')->filterByObjectid($_POST['id'])->delete();
+		$constructions = PlanningConstructionQuery::create()->filterByPlanningProjectId($_POST['id'])->find();
+		foreach($constructions as $construction){
+			PlanningActivityQuery::create()->filterByObjectType('Construction')->filterByObjectid($construction->getId())->delete();
+			$construction->delete();
+		}
     }
 }
