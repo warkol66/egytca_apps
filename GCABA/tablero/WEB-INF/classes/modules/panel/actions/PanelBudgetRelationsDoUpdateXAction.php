b888c5aa2fb5ca65940602ea447d653505bfcdb3
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-03-18 19:16:21 +0000

    *Modificaciones tabla y update sigaf

--- GCABA/tablero/WEB-INF/classes/modules/panel/actions/PanelBudgetRelationsDoUpdateXAction.php
+++ GCABA/tablero/WEB-INF/classes/modules/panel/actions/PanelBudgetRelationsDoUpdateXAction.php
@@ -50,20 +50,20 @@
 						<cp xmlns:d4p1="http://schemas.datacontract.org/2004/07/WCF_ENTIDADES" xmlns:i="http://www.w3.org/2001/XMLSchema-instance">
 							<d4p1:Actividad>'. $budget->getBudgetactivity() .'</d4p1:Actividad>
 							<d4p1:Ejercicio>'. $budget->getBudgetyear() .'</d4p1:Ejercicio>
-							<d4p1:Entidad>0</d4p1:Entidad>
-							<d4p1:FueFin>11</d4p1:FueFin>
-							<d4p1:Inciso>2</d4p1:Inciso>
+							<d4p1:Entidad>'. $budget->getBudgetEntity() .'</d4p1:Entidad>
+							<d4p1:FueFin>'. $budget->getBudgetSource() .'</d4p1:FueFin>
+							<d4p1:Inciso>'. $budget->getBudgetSubsection() .'</d4p1:Inciso>
 							<d4p1:Jurisdiccion>'. $budget->getBudgetJurisdiction() .'</d4p1:Jurisdiccion>
-							<d4p1:Moneda>1</d4p1:Moneda>
+							<d4p1:Moneda>'. $budget->getBudgetCurrency() .'</d4p1:Moneda>
 							<d4p1:Obra>'. $budget->getBudgetConstruction() .'</d4p1:Obra>
-							<d4p1:Parcial>2</d4p1:Parcial>
-							<d4p1:Principal>9</d4p1:Principal>
+							<d4p1:Parcial>'. $budget->getBudgetPartial() .'</d4p1:Parcial>
+							<d4p1:Principal>'. $budget->getBudgetCapital() .'</d4p1:Principal>
 							<d4p1:Programa>'. $budget->getBudgetProgram() .'</d4p1:Programa>
 							<d4p1:Proyecto>'. $budget->getBudgetProyect() .'</d4p1:Proyecto>
-							<d4p1:SubJurisdiccion>0</d4p1:SubJurisdiccion>
-							<d4p1:SubParcial>0</d4p1:SubParcial>
+							<d4p1:SubJurisdiccion>'. $budget->getBudgetSubjurisdiction() .'</d4p1:SubJurisdiccion>
+							<d4p1:SubParcial>'. $budget->getBudgetSubPartial() .'</d4p1:SubParcial>
 							<d4p1:SubPrograma>'. $budget->getBudgetSubProgram() .'</d4p1:SubPrograma>
-							<d4p1:UbicaGeo>1</d4p1:UbicaGeo>
+							<d4p1:UbicaGeo>'. $budget->getBudgetGeolocation .'</d4p1:UbicaGeo>
 						</cp>
 					</ConsultarPartidaPresupuestaria>
 				</s:Body>
@@ -75,6 +75,7 @@
 				//$webServiceBudget = new nusoap_client("http://10.73.2.136:83/wcfroot/servicio.svc?wsdl",true);
 				$err = $webServiceBudget->getError();
 				if ($err) {
+					$budget->setMatch(false)->save();
 					$smarty->assign("budget", $budget);
 					$smarty->assign("error", addslashes($err));
 					return $mapping->findForwardConfig('success');
@@ -94,12 +95,14 @@
 					$err = $webServiceBudget->getError();
 					if ($err) {
 						// Error
+						$budget->setUpdatedsigaf(date("Y-m-d H:i:s"));
+						$budget->setMatch(false)->save();
 						$smarty->assign("error", addslashes($err));
 					} else {
 						if(is_array($result)){
 							// Actualizo el registro en la base con $result
 							$budget->setMatch(true);
-							$budget->setUpdatedsigaf(date('now'));
+							$budget->setUpdatedsigaf(date("Y-m-d H:i:s"));
 							$budget->setDefinitive($result['ConsultarPartidaPresupuestariaResult']['Indicativa']['Definitivo']);
 							$budget->setAccrued($result['ConsultarPartidaPresupuestariaResult']['Indicativa']['Devengado']);
 							$budget->setPaid($result['ConsultarPartidaPresupuestariaResult']['Indicativa']['Pagado']);
--- GCABA/tablero/WEB-INF/classes/modules/panel/tpl/PanelBudgetRelationsRequest.tpl
+++ GCABA/tablero/WEB-INF/classes/modules/panel/tpl/PanelBudgetRelationsRequest.tpl
@@ -4,9 +4,4 @@
 	<div id="successMessage">Partidas actualizadas correctamente</div>
 |-/if-|
 
-<!--form action="Main.php" method="post">
-	<input type="hidden" name="do" value="panelBudgetRelationsDoRequest" />
-	<input type="submit" name="submit_go_update" value="Actualizar" title="Actualizar partidas presupuestarias"/>
-</form-->
-
 <input type='button' onClick="location.href='Main.php?do=panelBudgetRelationsDoRequest'" value="Actualizar" title="Actualizar partidas presupuestarias"/>
