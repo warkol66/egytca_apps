dbb7966dbeae0832fea72fd4b8567776ecb0ccdb
dcanuhe <dcanuhe@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-03-19 18:28:28 +0000

    panelExpensesShow & panelRelativeExpensesShow

--- trunk/GCABA/tablero/WEB-INF/classes/modules/panel/actions/PanelExpensesShowAction.php
+++ trunk/GCABA/tablero/WEB-INF/classes/modules/panel/actions/PanelExpensesShowAction.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'ExpensesSum.php';
+
 class PanelExpensesShowAction extends BaseAction {
 	
 	function execute($mapping, $form, &$request, &$response) {
@@ -18,158 +20,93 @@ class PanelExpensesShowAction extends BaseAction {
 		$smarty->assign("module",$module);
 		$smarty->assign("section",$section);
 		
-		
-		/* ***************** regionsExpenses ****************** */
-		$regionsExpenses = array();
-		$regions = RegionQuery::create()->filterByType(RegionPeer::COMMUNE)->find();
-		foreach ($regions as $region) {
-			
-			//----------------------------------------------------------------
-			/* las regiones tienen planningConstructions, pero las planningConstructions para conseguir
-			 * las budgetRelations filtran por el planningProject asociado a la planningConstruction.
-			 * No se repiten?
-			 */
-			$budgetRelations = array(ExpensesSum::createRandomBudgetRelation(), ExpensesSum::createRandomBudgetRelation()); // TODO: conseguir budgetRelations para la region
-			//----------------------------------------------------------------
-			
-			$regionExpenses = new ExpensesSum($region->getName(), $budgetRelations);
-			$regionsExpenses[] = $regionExpenses;
-		}
-		
+		$regionsExpenses = $this->getRegionsExpenses();
 		$regionsExpensesTotal = ExpensesSum::total($regionsExpenses);
-		
 		$smarty->assign('regionsExpenses', $regionsExpenses);
 		$smarty->assign('regionsExpensesTotal', $regionsExpensesTotal);
-		/* ************** fin regionsExpenses ***************** */
 		
-		
-		/* **************** ministriesExpenses **************** */
-		$ministriesExpenses = array();
-		$ministries = PositionQuery::findMinistries();
-		foreach ($ministries as $ministry) {
-			$budgetRelations = array(ExpensesSum::createRandomBudgetRelation()); // TODO: conseguir budgetRelations para la region
-			
-			$ministryExpenses = new ExpensesSum($ministry->getName(), $budgetRelations);
-			$ministriesExpenses[] = $ministryExpenses;
-		}
-		
+		$ministriesExpenses = $this->getMinistriesExpenses();		
 		$ministriesExpensesTotal = ExpensesSum::total($ministriesExpenses);
-		
 		$smarty->assign('ministriesExpenses', $ministriesExpenses);
 		$smarty->assign('ministriesExpensesTotal', $ministriesExpensesTotal);
-		/* ************* fin ministriesExpenses *************** */
 		
-		
-		/* **************** operativeObjectives *************** */
-		$operativeObjectivesExpenses = array();
-		$operativeObjectives = OperativeObjectiveQuery::create()->find();
-		foreach ($operativeObjectives as $operativeObjective) {
-			$budgetRelations = array(ExpensesSum::createRandomBudgetRelation()); // TODO: conseguir budgetRelations para la region
-			
-			$operativeObjectiveExpenses = new ExpensesSum($operativeObjective->getName(), $budgetRelations);
-			$operativeObjectivesExpenses[] = $operativeObjectiveExpenses;
-		}
-		
+		$operativeObjectivesExpenses = $this->getOperativeObjectivesExpenses();
 		$operativeObjectivesExpensesTotal = ExpensesSum::total($operativeObjectivesExpenses);
-		
 		$smarty->assign('operativeObjectivesExpenses', $operativeObjectivesExpenses);
 		$smarty->assign('operativeObjectivesExpensesTotal', $operativeObjectivesExpensesTotal);
-		/* ************* fin operativeobjectives ************** */
 		
-		
-		/* ***************** impactObjectives ***************** */
-		$impactObjectivesExpenses = array();
-		$impactObjectives = ImpactObjectiveQuery::create()->find();
-		foreach ($impactObjectives as $impactObjective) {
-			$budgetRelations = array(ExpensesSum::createRandomBudgetRelation()); // TODO: conseguir budgetRelations para la region
-			
-			$impactObjectiveExpenses = new ExpensesSum($impactObjective->getName(), $budgetRelations);
-			$impactObjectivesExpenses[] = $impactObjectiveExpenses;
-		}
-		
+		$impactObjectivesExpenses = $this->getImpactObjectivesExpenses();
 		$impactObjectivesExpensesTotal = ExpensesSum::total($impactObjectivesExpenses);
-		
 		$smarty->assign('impactObjectivesExpenses', $impactObjectivesExpenses);
 		$smarty->assign('impactObjectivesExpensesTotal', $impactObjectivesExpensesTotal);
-		/* ************** fin impactObjectives **************** */
 		
-		
-		/* **************** ministryObjectives **************** */
-		$ministryObjectivesExpenses = array();
-		$ministryObjectives = MinistryObjectiveQuery::create()->find();
-		foreach ($ministryObjectives as $ministryObjective) {
-			$budgetRelations = array(ExpensesSum::createRandomBudgetRelation()); // TODO: conseguir budgetRelations para la region
-			
-			$ministryObjectiveExpenses = new ExpensesSum($ministryObjective->getName(), $budgetRelations);
-			$ministryObjectivesExpenses[] = $ministryObjectiveExpenses;
-		}
-		
+		$ministryObjectivesExpenses = $this->getMinistryObjectivesExpenses();
 		$ministryObjectivesExpensesTotal = ExpensesSum::total($ministryObjectivesExpenses);
-		
 		$smarty->assign('ministryObjectivesExpenses', $ministryObjectivesExpenses);
 		$smarty->assign('ministryObjectivesExpensesTotal', $ministryObjectivesExpensesTotal);
-		/* ************* fin ministryObjectives *************** */
-		
 		
 		return $mapping->findForwardConfig('success');
 	}
-}
-
-// TODO: mover
-class ExpensesSum {
 	
-	private $entityName;
-	private $sumByType;
-	static private $budgetFields = array(
-		'sanctioned', 'active', 'restricted', 'preventive',
-		'definitive', 'accrued', 'available', 'paid'
-	);
-	
-	public function __construct($entityName, $budgetRelations) {
-		$this->entityName = $entityName;
-		$this->calculateSumByType($budgetRelations);
+	/**
+	 * Returns a collection with the ExpensesSum of the BudgetRelations asociated to the Comunes
+	 * @return array array of ExpensesSum
+	 */
+	function getRegionsExpenses() {
+		$regions = RegionQuery::create()->filterByType(RegionPeer::COMMUNE)->find();
+		return $this->getEntitiesExpenses($regions);
 	}
 	
-	public function getEntityName() {
-		return $this->entityName;
+	/**
+	 * Returns a collection with the ExpensesSum of the BudgetRelations asociated to the Ministries
+	 * @return array array of ExpensesSum
+	 */
+	function getMinistriesExpenses() {
+		$ministries = PositionQuery::findMinistries();
+		return $this->getEntitiesExpenses($ministries);
 	}
 	
-	public function __call($name, $arguments) {
-		// getXxx() para todos los valores
-		$budgetField = strtolower(preg_replace("/^get/", '', $name));
-		if (in_array($budgetField, self::$budgetFields))
-			return $this->sumByType[$budgetField];
+	/**
+	 * Returns a collection with the ExpensesSum of the BudgetRelations asociated to the OperativeObjectives
+	 * @return array array of ExpensesSum
+	 */
+	function getOperativeObjectivesExpenses() {
+		$operativeObjectives = OperativeObjectiveQuery::create()->find();
+		return $this->getEntitiesExpenses($operativeObjectives);
 	}
 	
-	public static function total($expensesSums) {
-		$total = array();
-		foreach (self::$budgetFields as $budgetField) {
-			foreach ($expensesSums as $expensesSum) {
-				$getFieldMethod = "get$budgetField";
-				$total[$budgetField] += $expensesSum->$getFieldMethod();
-			}
+	/**
+	 * Returns a collection with the ExpensesSum of the BudgetRelations asociated to the ImpactObjectives
+	 * @return array array of ExpensesSum
+	 */
+	function getImpactObjectivesExpenses() {
+		$impactObjectives = ImpactObjectiveQuery::create()->find();
+		return $this->getEntitiesExpenses($impactObjectives);
+	}
+	
+	/**
+	 * Returns a collection with the ExpensesSum of the BudgetRelations asociated to the MinistryObjectives
+	 * @return array array of ExpensesSum
+	 */
+	function getMinistryObjectivesExpenses() {
+		$ministryObjectives = MinistryObjectiveQuery::create()->find();
+		return $this->getEntitiesExpenses($ministryObjectives);
+	}
+	
+	/**
+	 * Returns a collection with the ExpensesSum of the BudgetRelations asociated to the $entities
+	 * @return array array of ExpensesSum
+	 */
+	function getEntitiesExpenses($entities) {
+		$entitiesExpenses = array();
+		foreach ($entities as $entity) {
+			
+			$budgetRelations = $entity->getBudgetItems();
+//			$budgetRelations = array(ExpensesSum::createRandomBudgetRelation(), ExpensesSum::createRandomBudgetRelation()); // TODO: revisar $budgetRelations y borrar
+			
+			$entityExpenses = new ExpensesSum($entity->getName(), $budgetRelations);
+			$entitiesExpenses[] = $entityExpenses;
 		}
-		return $total;
-	}
-	
-	private function calculateSumByType($budgetRelations) {
-		$this->sumByType = array();
-		foreach ($budgetRelations as $budgetRelation) {
-			foreach (self::$budgetFields as $budgetField) {
-				$getFieldMethod = "get$budgetField";
-				$this->sumByType[$budgetField] += $budgetRelation->$getFieldMethod();
-			}
-		}
-	}
-	
-	
-	// DELETEME
-	static function createRandomBudgetRelation() {
-		$budgetRelation = new BudgetRelation();
-		foreach (self::$budgetFields as $budgetField) {
-			$setFieldMethod = "set$budgetField";
-			$budgetRelation->$setFieldMethod(rand(0, 9));
-		}
-		return $budgetRelation;
+		return $entitiesExpenses;
 	}
 }
--- trunk/GCABA/tablero/WEB-INF/classes/modules/panel/actions/PanelRelativeExpensesShowAction.php
+++ trunk/GCABA/tablero/WEB-INF/classes/modules/panel/actions/PanelRelativeExpensesShowAction.php
@@ -1,6 +1,9 @@
 <?php
 
-class PanelRelativeExpensesShowAction extends BaseAction {
+require_once 'ExpensesSum.php';
+require_once 'PanelExpensesShowAction.php';
+
+class PanelRelativeExpensesShowAction extends PanelExpensesShowAction {
 	
 	function execute($mapping, $form, &$request, &$response) {
 
@@ -17,6 +20,31 @@ class PanelRelativeExpensesShowAction extends BaseAction {
 
 		$smarty->assign("module",$module);
 		$smarty->assign("section",$section);
+		
+		$regionsExpenses = RelativeExpensesSum::absoluteToRelative($this->getRegionsExpenses());
+//		$regionsExpensesTotal = RelativeExpensesSum::total($regionsExpenses);
+		$smarty->assign('regionsExpenses', $regionsExpenses);
+//		$smarty->assign('regionsExpensesTotal', $regionsExpensesTotal);
+		
+		$ministriesExpenses = RelativeExpensesSum::absoluteToRelative($this->getMinistriesExpenses());
+//		$ministriesExpensesTotal = RelativeExpensesSum::total($ministriesExpenses);
+		$smarty->assign('ministriesExpenses', $ministriesExpenses);
+//		$smarty->assign('ministriesExpensesTotal', $ministriesExpensesTotal);
+		
+		$operativeObjectivesExpenses = RelativeExpensesSum::absoluteToRelative($this->getOperativeObjectivesExpenses());
+//		$operativeObjectivesExpensesTotal = RelativeExpensesSum::total($operativeObjectivesExpenses);
+		$smarty->assign('operativeObjectivesExpenses', $operativeObjectivesExpenses);
+//		$smarty->assign('operativeObjectivesExpensesTotal', $operativeObjectivesExpensesTotal);
+		
+		$impactObjectivesExpenses = RelativeExpensesSum::absoluteToRelative($this->getImpactObjectivesExpenses());
+//		$impactObjectivesExpensesTotal = RelativeExpensesSum::total($impactObjectivesExpenses);
+		$smarty->assign('impactObjectivesExpenses', $impactObjectivesExpenses);
+//		$smarty->assign('impactObjectivesExpensesTotal', $impactObjectivesExpensesTotal);
+		
+		$ministryObjectivesExpenses = RelativeExpensesSum::absoluteToRelative($this->getMinistryObjectivesExpenses());
+//		$ministryObjectivesExpensesTotal = RelativeExpensesSum::total($ministryObjectivesExpenses);
+		$smarty->assign('ministryObjectivesExpenses', $ministryObjectivesExpenses);
+//		$smarty->assign('ministryObjectivesExpensesTotal', $ministryObjectivesExpensesTotal);
 
 		return $mapping->findForwardConfig('success');
 	}
