d0d80eaa3e9bda4344c7669158e54f393ad67de2
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-03-20 14:25:24 +0000

    * Agrego el pasaje de un parametro "class" en caso que el campo que se le quiere poner eadonly ya tenga un estilo por ejemplo emptyValidation o de ancho determinado, al tener dos declaraciones de clase se rompía. Modo de uso:  * Example:  |-$readonly|readonly-| o |-$readonly|readonly:"emptyValidation right"-|

--- externals/lib-phpmvc/smarty3/plugins/modifier.readonly.php
+++ externals/lib-phpmvc/smarty3/plugins/modifier.readonly.php
@@ -6,10 +6,15 @@
  * Type:     modifier
  * Name:     readonly
  * Purpose:  Devuelve el readonly="readonly" class="readonly" si el valor es show o readonly
+ * Example:  |-$readonly|readonly-| o |-$readonly|readonly:"emptyValidation right"-|
  * -------------------------------------------------------------
  */
-function smarty_modifier_readonly($value){
+function smarty_modifier_readonly($value, $class = '') {
+	if ($class != '')
+		$separator = ' ';
 	if ($value == "readonly" || $value == "showLog")
-		return 'readonly="readonly" class="readOnly" onclick="return false" onkeydown="return false"';
-	return '';
+		return 'readonly="readonly" class="' . $class . $separator . 'readOnly" onclick="return false" onkeydown="return false"';
+	if ($class != '')
+		$class = 'class="' . $class . '"';
+	return $class;
 }
