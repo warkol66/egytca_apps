961e2d78dd4ea1359e9b1e867dc322816a191392
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-03-28 04:49:24 +0000

    *Guardado de partidas presupuestarias por ajax

---
+++ GCABA/tablero/WEB-INF/classes/modules/panel/actions/PanelBudgetRelationsDoEditXAction.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * PanelBudgetRelationsDoEditXAction
+ *
+ * Crea o guarda cambios de Partidas Presupuestarias (BudgetRelations)
+ *
+ * @package    panel
+ * @subpackage    panelBudgetRelations
+ */
+
+class PanelBudgetRelationsDoEditXAction extends BaseAction {
+
+	function PanelBudgetRelationsDoEditXAction() {
+		;
+	}
+
+	function execute($mapping, $form, &$request, &$response) {
+
+		BaseAction::execute($mapping, $form, $request, $response);
+
+		$plugInKey = 'SMARTY_PLUGIN';
+		$smarty =& $this->actionServer->getPlugIn($plugInKey);
+		if($smarty == NULL) {
+			echo 'No PlugIn found matching key: '.$plugInKey."<br>\n";
+		}
+		
+		foreach ($_POST["budgetItem"] as $item) {
+			foreach ($item as $itemValue => $value) {
+				if ($itemValue == "amount") 
+					$value = Common::convertToMysqlNumericFormat($value);
+				$itemValues[$itemValue] = $value;
+			}
+		}
+		
+		if(!empty($itemValues["id"])) {
+			$budgetRelation = BudgetRelationQuery::create()->findOneById($itemValues["id"]);
+			if (empty($budgetRelation))
+				$budgetRelation = new BudgetRelation();
+			else
+				$budgetRelation = new BudgetRelation();
+			
+			$budgetRelation->fromArray($itemValues,BasePeer::TYPE_FIELDNAME);
+			$budgetRelation->setObjectType($_POST["objectType"]);
+			$budgetRelation->setObjectid($_POST["objectId"]);
+			try {
+				$budgetRelation->save();
+			} catch (PropelException $exp) {
+				if (ConfigModule::get("global","showPropelExceptions"))
+					print_r($exp->__toString());
+			}
+			$smarty->assign("bEdit", "ok");
+			return $mapping->findForwardConfig('success');
+		}
+		
+		$smarty->assign("bEdit", "error");
+		return $mapping->findForwardConfig('success');
+		
+	}
+	
+}
+	
+
---
+++ GCABA/tablero/WEB-INF/classes/modules/panel/tpl/PanelBudgetRelationsDoEditX.tpl
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+|-if $bEdit eq "ok"-|
+	$('budgetItemMsgField').innerHTML = '<span class="">Partida actualizada con éxito</span>';
+|-else-|
+	$('budgetItemMsgField').innerHTML = '<span class="">Ocurrió un error al intentar actualizar la partida</span>';
+|-/if-|
+</script>
