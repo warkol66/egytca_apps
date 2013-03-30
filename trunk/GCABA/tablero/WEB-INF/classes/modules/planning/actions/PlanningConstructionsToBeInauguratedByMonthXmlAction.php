b7eff8885a60539fe4e9ff8cde6037af6d0f89ec
dcanuhe <dcanuhe@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-03-30 04:39:44 +0000

    de-harcodeado de años para PlanningConstructionsToBeInauguratedByMonthXml

--- trunk/GCABA/tablero/WEB-INF/classes/modules/planning/actions/PlanningConstructionsToBeInauguratedByMonthXmlAction.php
+++ trunk/GCABA/tablero/WEB-INF/classes/modules/planning/actions/PlanningConstructionsToBeInauguratedByMonthXmlAction.php
@@ -17,20 +17,33 @@ class PlanningConstructionsToBeInauguratedByMonthXmlAction extends BaseAction {
 		$positions = PositionQuery::findMinistries();
 		
 		$months = array();
-		for ($i = 1; $i <= 12; $i++) {	
-			
-			$monthStr = "2013-$i";
-			$month['label'] = date('M', strtotime($monthStr)).'13';
-			$month['dateRange']['min'] = strtotime("first day of $monthStr");
-			$month['dateRange']['max'] = strtotime("last day of $monthStr");
-			$month['value'] = 0;
-			
-			foreach ($positions as $position) {
-				$dateFilteredQuery = PlanningConstructionQuery::create()->filterByPotentialendingdate($month['dateRange']);
-				$month['value'] += count($position->getPlanningConstructionsWithDescendants($dateFilteredQuery));
-			}
 		
-			$months[] = $month;
+		$firstYear = ConfigModule::get('planning', 'startingYear');
+		$lastYear = ConfigModule::get('planning', 'endingYear');
+		$years = array();
+		for ($yearIndex = $firstYear; $yearIndex <= $lastYear; $yearIndex++) {
+			$years[] = $yearIndex;
+		}
+		
+		foreach ($years as $fourDigitsYear) {
+			
+			$twoDigitsYear = preg_replace("/^[0-9]{2}/", '', $fourDigitsYear);
+			
+			for ($i = 1; $i <= 12; $i++) {
+
+				$monthStr = "$fourDigitsYear-$i";
+				$month['label'] = date('M', strtotime($monthStr)).$twoDigitsYear;
+				$month['dateRange']['min'] = strtotime("first day of $monthStr");
+				$month['dateRange']['max'] = strtotime("last day of $monthStr");
+				$month['value'] = 0;
+
+				foreach ($positions as $position) {
+					$dateFilteredQuery = PlanningConstructionQuery::create()->filterByPotentialendingdate($month['dateRange']);
+					$month['value'] += count($position->getPlanningConstructionsWithDescendants($dateFilteredQuery));
+				}
+
+				$months[] = $month;
+			}
 		}
 		
 		$smarty->assign('months', $months);
