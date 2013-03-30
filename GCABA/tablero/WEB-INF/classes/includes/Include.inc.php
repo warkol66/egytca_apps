47ba3b44d4885a2ed1221f417800cb4cd32abec8
dcanuhe <dcanuhe@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-03-30 04:43:56 +0000

    mensaje de deprecado preguntando si existe ConfigModule

--- GCABA/tablero/WEB-INF/classes/includes/Include.inc.php
+++ GCABA/tablero/WEB-INF/classes/includes/Include.inc.php
@@ -127,7 +127,7 @@
 		if ($errno == E_USER_ERROR || $errno == E_ERROR || $errno == E_CORE_ERROR  ||
 				$errno == E_COMPILE_ERROR || $errno == mysql_errno()) {
 
-			if(($system['config']['system']['parameters']['debugMode']['value']=='YES')) {
+			if(class_exists('ConfigModule') && ConfigModule::get('global', 'debugMode')) {
 
 				require_once('EmailManagement.php');
 				$manager = new EmailManagement();
@@ -154,6 +154,10 @@
 
 			die("<br /><strong><span style='color:red'>Error procesando su requerimiento, por favor reintente o comuniquese con el administrador.</span></strong>\n <br /><br />".
 					"Texto del error:" . $err );
+		}
+		elseif (class_exists('ConfigModule') && ConfigModule::get('global', 'debugMode') && ConfigModule::get('global', 'warnDeprecated')
+				&& $errno == E_USER_DEPRECATED) {
+			echo $err;
 		}
 	}
 
