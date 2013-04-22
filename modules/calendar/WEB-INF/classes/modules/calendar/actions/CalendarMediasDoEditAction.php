15631fc9e2b19f94ee66732e91bc3cb6cb88e59a
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-04-22 09:38:50 +0000

    *Viejo MediasDoEdit

--- modules/calendar/WEB-INF/classes/modules/calendar/actions/CalendarMediasDoEditAction.php
+++ modules/calendar/WEB-INF/classes/modules/calendar/actions/CalendarMediasDoEditAction.php
@@ -1,36 +1,87 @@
 <?php
 
-class CalendarMediasDoEditAction extends BaseDoEditAction {
-	
-	function __construct() {
-		parent::__construct('CalendarMedia');
-	}
-	
-	protected function postUpdate() {
-		parent::postUpdate();
-		
-		$this->smarty->assign("module","CalendarMedias");
-		
-	}
-	
-	protected function postSave() {
-		parent::postSave();
-		
-		$destPath = CalendarMedia::getSavePath();
-		$destPath .= $this->entity->getId();
+require_once("BaseAction.php");
+require_once("CalendarMediaPeer.php");
 
-		//Guardar el archivo
-		if (!empty($_FILES["document_file"]['tmp_name']))
-			CalendarMedia::createImages($this->entity,$_FILES['document_file'],$this->entity->getId());
-		
-		/* Ver esto*/
-		global $system;
-  
-		$saveOriginalFiles = $system["config"]["calendar"]["medias"]["saveOriginalFiles"]["value"];
+class CalendarMediasDoEditAction extends BaseAction {
 
-		if ($saveOriginalFiles == "YES")
-		  copy($file["tmp_name"],$destPath);
+
+	// ----- Constructor ---------------------------------------------------- //
+
+	function CalendarMediasDoEditAction() {
+		;
+	}
+
+
+	// ----- Public Methods ------------------------------------------------- //
+
+	/**
+	* Process the specified HTTP request, and create the corresponding HTTP
+	* response (or forward to another web component that will create it).
+	* Return an <code>ActionForward</code> instance describing where and how
+	* control should be forwarded, or <code>NULL</code> if the response has
+	* already been completed.
+	*
+	* @param ActionConfig		The ActionConfig (mapping) used to select this instance
+	* @param ActionForm			The optional ActionForm bean for this request (if any)
+	* @param HttpRequestBase	The HTTP request we are processing
+	* @param HttpRequestBase	The HTTP response we are creating
+	* @public
+	* @returns ActionForward
+	*/
+	function execute($mapping, $form, &$request, &$response) {
+
+    BaseAction::execute($mapping, $form, $request, $response);
+
+		//////////
+		// Access the Smarty PlugIn instance
+		// Note the reference "=&"
+		$plugInKey = 'SMARTY_PLUGIN';
+		$smarty =& $this->actionServer->getPlugIn($plugInKey);
+		if($smarty == NULL) {
+			echo 'No PlugIn found matching key: '.$plugInKey."<br>\n";
+		}
+
+		$module = "CalendarMedias";
+		$smarty->assign("module",$module);
+				
+
+		if ( $_POST["action"] == "edit" ) {
+			//estoy editando un calendarmedia existente
+
+			CalendarMediaPeer::update($_POST["calendarMedia"]);
+
+		}
+		else {
+		  //estoy creando un nuevo calendarmedia
+
+			if ( !CalendarMediaPeer::create($_POST["calendarMedia"]) ) {
+				$calendarMedia = new CalendarMedia();
+			$calendarMedia->setid($_POST["calendarMedia"]["id"]);
+			$calendarMedia->setcalendarEventId($_POST["calendarMedia"]["calendarEventId"]);
+			require_once("CalendarEventPeer.php");		
+			$smarty->assign("calendarEventIdValues",CalendarEventPeer::getAll());
+			$calendarMedia->setname($_POST["calendarMedia"]["name"]);
+			$calendarMedia->settitle($_POST["calendarMedia"]["title"]);
+			$calendarMedia->setdescription($_POST["calendarMedia"]["description"]);
+			$calendarMedia->setmediaType($_POST["calendarMedia"]["mediaType"]);
+			$calendarMedia->setorder($_POST["calendarMedia"]["order"]);
+			$calendarMedia->setcreationDate($_POST["calendarMedia"]["creationDate"]);
+			$calendarMedia->setstatus($_POST["calendarMedia"]["status"]);
+			$calendarMedia->setuserId($_POST["calendarMedia"]["userId"]);
+			require_once("UserPeer.php");		
+			$smarty->assign("userIdValues",UserPeer::getAll());
+				$smarty->assign("calendarMedia",$calendarMedia);	
+				$smarty->assign("action","create");
+				$smarty->assign("message","error");
+				return $this->addFiltersToForwards($_POST['filters'],$mapping,'failure');
+      		}
+			
+		}
+		
+		//redireccionamos con las opciones correctas
+		return $this->addFiltersToForwards($_POST['filters'],$mapping,'success');
 
 	}
-	
+
 }
