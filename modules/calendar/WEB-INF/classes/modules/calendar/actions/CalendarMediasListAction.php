1fee0a6cbbf8e32ac482744bb08cb909be6ad2c1
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-04-22 09:44:47 +0000

    *Version ant

--- modules/calendar/WEB-INF/classes/modules/calendar/actions/CalendarMediasListAction.php
+++ modules/calendar/WEB-INF/classes/modules/calendar/actions/CalendarMediasListAction.php
@@ -1,45 +1,89 @@
 <?php
 
-class CalendarMediasListAction extends BaseListAction {
-	
-	function __construct() {
-		parent::__construct('CalendarMedia');
+require_once("BaseAction.php");
+require_once("CalendarEventPeer.php");
+require_once("CalendarMediaPeer.php");
+
+class CalendarMediasListAction extends BaseAction {
+
+
+	// ----- Constructor ---------------------------------------------------- //
+
+	function CalendarMediasListAction() {
+		;
 	}
-	
-	protected function preList() {
-		parent::preList();
-		
-		if(!empty($_GET['filters']['minDate']) || !empty($_GET['filters']['maxDate']))
-			unset($this->filters['dateRange']);
-		if(!empty($_GET['filters']['minDate'])){
-            $this->filters['dateRange']['creationdate']['min'] = $_GET['filters']['minDate'];
-        }
-        if(!empty($_GET['filters']['maxDate'])){
-            $this->filters['dateRange']['creationdate']['max'] = $_GET['filters']['maxDate'];
+
+
+	// ----- Public Methods ------------------------------------------------- //
+
+	/**
+	* Process the specified HTTP request, and create the corresponding HTTP
+	* response (or forward to another web component that will create it).
+	* Return an <code>ActionForward</code> instance describing where and how
+	* control should be forwarded, or <code>NULL</code> if the response has
+	* already been completed.
+	*
+	* @param ActionConfig		The ActionConfig (mapping) used to select this instance
+	* @param ActionForm			The optional ActionForm bean for this request (if any)
+	* @param HttpRequestBase	The HTTP request we are processing
+	* @param HttpRequestBase	The HTTP response we are creating
+	* @public
+	* @returns ActionForward
+	*/
+	function execute($mapping, $form, &$request, &$response) {
+
+    BaseAction::execute($mapping, $form, $request, $response);
+
+		//////////
+		// Access the Smarty PlugIn instance
+		// Note the reference "=&"
+		$plugInKey = 'SMARTY_PLUGIN';
+		$smarty =& $this->actionServer->getPlugIn($plugInKey);
+		if($smarty == NULL) {
+			echo 'No PlugIn found matching key: '.$plugInKey."<br>\n";
 		}
-		
-	}
-	
-	protected function postList(){
-		parent::postList();
-		
+
 		$module = "CalendarMedias";
-		$this->smarty->assign("module",$module);
+		$smarty->assign("module",$module);
 		
+		//seteamos lo necesario para crear la interfaz de filtros
 		$user = Common::getAdminLogged();
+		//$categories = $user->getCategoriesByModule('news');
 		$categories = $user->getCategories();
-		$this->smarty->assign("categories",$categories);
-		//Cambiar al migrar peer
-		$this->smarty->assign("mediaTypes",CalendarMediaPeer::getMediaTypes());
+		$smarty->assign("categories",$categories);
+		$smarty->assign("mediaTypes",CalendarMediaPeer::getMediaTypes());
+		
+		$calendarMediaPeer = new CalendarMediaPeer();
 		
 		//filtros
-		if(!empty($_GET['filters']['dateRange']['creationdate']['min']))
-            $this->filters['minDate'] = $_GET['filters']['dateRange']['creationdate']['min'];
-        if(!empty($_GET['filters']['dateRange']['creationdate']['max']))
-            $this->filters['maxDate'] = $_GET['filters']['dateRange']['creationdate']['max'];
-            
-        $this->smarty->assign("filters",$this->filters);
+		if (isset($_GET['filters'])) {
+			$smarty->assign('filters',$_GET['filters']);
+			
+			if (!empty($_GET['filters']['categoryId'])) {
+				$category = CategoryPeer::get($_GET['filters']['categoryId']);
+				$calendarMediaPeer->setCategory($category);			
+			}			
+				
+		}					
+ 
+		$pager = $calendarMediaPeer->getAllPaginatedFiltered($_GET["page"]);
+		$smarty->assign("calendarMedias",$pager->getResult());
+		$smarty->assign("pager",$pager);
 		
+		$url = "Main.php?do=calendarMediasList";
+		
+		if (isset($_GET['page']))
+			$url .= '&page=' . $_GET['page'];
+		
+		//aplicacion de filtro a url
+		foreach ($_GET['filters'] as $key => $value)
+			$url .= "&filters[$key]=$value";
+		
+		$smarty->assign("url",$url);		
+   
+		$smarty->assign("message",$_GET["message"]);
+
+		return $mapping->findForwardConfig('success');
 	}
 
 }
