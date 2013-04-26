8e4a900455505abc9da4582ecea8df835bdbd0f0
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-04-26 05:30:04 +0000

    *Fin bloqueo de ips y usuarios

--- trunk/ecuador/WEB-INF/classes/modules/common/actions/CommonUsersDoUnblockXAction.php
+++ trunk/ecuador/WEB-INF/classes/modules/common/actions/CommonUsersDoUnblockXAction.php
@@ -4,33 +4,24 @@
  *
  * @package users 
  */
+ 
+class CommonUsersDoUnblockXAction extends BaseDoEditAction {
 
-class CommonUsersDoUnblockXAction extends BaseAction {
-
-	function CommonUsersDoUnblockXAction() {
-		;
+	function __construct() {
+		parent::__construct($_POST['params']['type']);
 	}
-
-	function execute($mapping, $form, &$request, &$response) {
-
-		BaseAction::execute($mapping, $form, $request, $response);
-
-		//////////
-		// Access the Smarty PlugIn instance
-		// Note the reference "=&"
-		$plugInKey = 'SMARTY_PLUGIN';
-		$smarty =& $this->actionServer->getPlugIn($plugInKey);
-		if($smarty == NULL) {
-			echo 'No PlugIn found matching key: '.$plugInKey."<br>\n";
-		}
+	
+	protected function preUpdate(){
+		parent::preUpdate();
 		
-		if(Common::unblockUser($_POST["type"], $_POST["id"])) {
-			$smarty->assign("message","success");
-			$smarty->assign("id",$_POST["id"]);
-		} else
-			$smarty->assign("message","error");
-
-		return $mapping->findForwardConfig('success');
+		$this->entityParams['blockedAt'] = null;
 	}
-
+	
+	protected function postUpdate(){
+		parent::postUpdate();
+		
+		$this->smarty->assign("divId", $_POST['params']['type']);
+		$this->smarty->assign("userId", $this->entity->getId());
+	}
+	
 }
--- trunk/ecuador/WEB-INF/classes/modules/common/tpl/CommonUsersDoUnblockX.tpl
+++ trunk/ecuador/WEB-INF/classes/modules/common/tpl/CommonUsersDoUnblockX.tpl
@@ -1,8 +1,4 @@
-<script type="text/javascript">
-|-if $message eq 'success'-|
-	$('#usersMsgField').html('<span class="resultSuccess">Usuario desbloqueado</span>');
-	$('#unblock_|-$id-|').remove();
-|-else-|
-	$('#usersMsgField').html('<span class="resultError">Ocurrió un error al desbloquear al usuario. Intente nuevamente</span>');
-|-/if-|
+<script type="text/javascript" language="javascript">
+	$('#|-$divId-|_|-$userId-|').remove();
 </script>
+<span class="resultSuccess">Usuario desbloqueado</span>
