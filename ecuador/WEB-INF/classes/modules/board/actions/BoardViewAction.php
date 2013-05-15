b50b4d22150f0a2b4ca91271f28ac2e51c1a5b55
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-05-15 05:40:33 +0000

    *Arreglos a board para los bonds

--- ecuador/WEB-INF/classes/modules/board/actions/BoardViewAction.php
+++ ecuador/WEB-INF/classes/modules/board/actions/BoardViewAction.php
@@ -29,16 +29,25 @@ class BoardViewAction extends BaseEditAction {
 		$moduleConfig = Common::getModuleConfiguration("board");
 		$this->smarty->assign("moduleConfig",$moduleConfig);
 		
-		//if ($moduleConfig['comments']['useComments']['value'] == "YES") {
-			//busco los compromisos posibles
-			$bonds = BoardBondQuery::create()->find();
-			$this->smarty->assign("bonds",$bonds);
+		//busco los compromisos posibles
+		$this->smarty->assign("bonds",BoardBond::getTypes());
+		//busco los compromisos existentes en este desafio
+		$usersB = BoardBondQuery::create()->filterByChallengeId($this->entity->getId())->find();
+		$usersBonds = array();
+		foreach($usersB as $usersBond){
+			$usersBonds[] = $usersBond->getType();
+		}
+		
+		$this->smarty->assign("usersBonds",$usersBonds);
+		
+		if ($moduleConfig['comments']['useComments']['value'] == "YES") {
+
 			//si se la configuracion pide que se muestren los comentarios de forma directa
-			//if ($moduleConfig['comments']['displayComments']['value'] == 'YES') {
+			if ($moduleConfig['comments']['displayComments']['value'] == 'YES') {
 				$comments = BoardCommentQuery::create()->filterByParentId(NULL, Criteria::EQUAL)->findByChallengeIdAndStatus($this->entity->getId(),BoardComment::APPROVED);
 				$this->smarty->assign("comments",$comments);
-			//}
-		//}
+			}
+		}
 		
 		$this->smarty->assign("challengeDeleted", $this->entity->getDeletedAt('Y-m-d H:i:s')); 
 		
