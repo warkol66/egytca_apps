829f7f65d2567c0e2cf23117e850390e71b73eb0
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-05-15 00:44:42 +0000

    *Arreglo para comments en blog

--- trunk/ecuador/WEB-INF/classes/modules/blog/actions/BlogViewAction.php
+++ trunk/ecuador/WEB-INF/classes/modules/blog/actions/BlogViewAction.php
@@ -37,6 +37,10 @@ class BlogViewAction extends BaseEditAction {
 			}
 		}
 		
+		if(isset($_SESSION['loginUser']) || isset($_SESSION['loginAffiliateUser']) || isset($_SESSION['loginClientUser']))
+			$logged = true;
+		
+		$this->smarty->assign("logged", $logged); 
 		$this->smarty->assign("entryDeleted", $this->entity->getDeletedAt('Y-m-d H:i:s')); 
 		
 		$this->template->template = "TemplateBlogPublic.tpl";
