e13381c344d27d49dc723787223f2beb0d19c26b
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-05-15 11:11:30 +0000

    *Control de bonds duplicados

--- trunk/ecuador/WEB-INF/classes/modules/board/actions/BoardDoAddBondToChallengeXAction.php
+++ trunk/ecuador/WEB-INF/classes/modules/board/actions/BoardDoAddBondToChallengeXAction.php
@@ -11,25 +11,44 @@ class BoardDoAddBondToChallengeXAction extends BaseDoEditAction {
 		
 		if(isset($_POST['bondId']) && isset($_POST['challengeId'])){
 			
-			if(isset($_SESSION['loginUser'])){
+			if(isset($_SESSION['loginUser']))
 				$user = $_SESSION['loginUser'];
-				$this->entityParams['userId'] = $user->getId();
-				$this->entityParams['userType'] = get_class($user);
-			}
-			elseif(isset($_SESSION['loginAffiliateUser'])){
+			elseif(isset($_SESSION['loginAffiliateUser']))
 				$user = $_SESSION['loginAffiliateUser'];
-				$this->entityParams['userId'] = $user->getId();
-				$this->entityParams['userType'] = get_class($user);
-			}
-			elseif(isset($_SESSION['loginClientUser'])){
+			elseif(isset($_SESSION['loginClientUser']))
 				$user = $_SESSION['loginClientUser'];
-				$this->entityParams['userId'] = $user->getId();
-				$this->entityParams['userType'] = get_class($user);
-			}
 			
+			$this->entityParams['userId'] = $user->getId();
+			$this->entityParams['userType'] = get_class($user);
 			$this->entityParams['type'] = $_POST['bondId'];
 			$this->entityParams['challengeId'] = $_POST['challengeId'];
+			
+			//me fijo que no exista otro bond con estos datos
+			$existent = BoardBondQuery::create()
+			->filterByChallengeId($_POST['challengeId'])
+			->filterByType($_POST['bondId'])
+			->filterByUserId($user->getId())
+			->filterByUserType(get_class($user))
+			->findOne();
+		
+			if(is_object($existent)){
+				//busco los compromisos posibles
+				$this->smarty->assign("bonds",BoardBond::getTypes());
+				//busco los compromisos existentes en este desafio
+				$usersB = BoardBondQuery::create()->filterByChallengeId($_POST['challengeId'])->find();
+				$usersBonds = array();
+				foreach($usersB as $usersBond){
+					$usersBonds[] = $usersBond->getType();
+				}
+				
+				$this->smarty->assign("usersBonds",$usersBonds);
+				
+				$this->smarty->assign("existent",true);
+				$this->forwardFailureName = 'success';
+				return false;
+			}
 		}
+		
 	}
 	
 	protected function postSave(){
