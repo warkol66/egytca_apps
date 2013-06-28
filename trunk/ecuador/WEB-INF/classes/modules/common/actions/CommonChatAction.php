2b21d16f4409aeccf8f21f084c7ef5953ce06689
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-06-28 19:05:46 +0000

    * Ajustes al action que inicia el chat

--- trunk/ecuador/WEB-INF/classes/modules/common/actions/CommonChatAction.php
+++ trunk/ecuador/WEB-INF/classes/modules/common/actions/CommonChatAction.php
@@ -12,9 +12,7 @@ class CommonChatAction extends BaseAction {
 			echo 'No PlugIn found matching key: '.$plugInKey."<br>\n";
 		}
 		
-		$loggedUser = Common::getLoggedUser();
-		$smarty->assign('userName', $loggedUser->getUserName());
-		$smarty->assign('password', $loggedUser->getPassword());
 		return $mapping->findForwardConfig('success');
+
 	}
 }
