4e0945ee772f6abcfda7d34970343519f7e2241e
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-07-16 04:54:52 +0000

    *logData para users

--- ecuador/WEB-INF/classes/modules/users/classes/Group.php
+++ ecuador/WEB-INF/classes/modules/users/classes/Group.php
@@ -29,5 +29,13 @@ class Group extends BaseGroup {
 		return $categories;
 
 	}
+	
+	/**
+	* Devuelve el string para ser usado en el historico de operaciones
+	*	@return string con el texto a guardar en el historico de operaciones
+	*/
+	public function getLogData(){
+		return substr($this->getName(),0,50);
+	}
 
 }
