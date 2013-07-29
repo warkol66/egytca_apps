e3cb7cb84fc04684d5cc561687d91d2bf5147f64
dcanuhe <dcanuhe@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-07-29 06:06:36 +0000

    agrego Prepend.php

---
+++ externals/phpMVC5.4/WEB-INF/Prepend.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Prepend
+*
+* $author Modulos Empresarios / Egytca
+* @package phpMVCconfig
+*/
+
+//ponemos el server en UTC
+date_default_timezone_set('UTC');
+
+require_once("BaseAction.php");
