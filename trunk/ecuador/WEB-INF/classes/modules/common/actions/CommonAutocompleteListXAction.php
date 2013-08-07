ad5cc5dc18e36d14344ea2d51af683dd2881047a
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-08-07 08:52:09 +0000

    seleccion de destinatarios con autocomplete por ajax

--- trunk/ecuador/WEB-INF/classes/modules/common/actions/CommonAutocompleteListXAction.php
+++ trunk/ecuador/WEB-INF/classes/modules/common/actions/CommonAutocompleteListXAction.php
@@ -25,6 +25,9 @@ class CommonAutocompleteListXAction extends BaseAction {
 		$object = ucfirst($request->getParameter('object'));
 
 		$filters = array("searchString" => $searchString, "limit" => $_REQUEST['limit']);
+		
+		/*if ($_REQUEST['getCandidates'])
+			$filters = array_merge_recursive($filters, array("getCandidates" => true));*/
 
 		$objects = BaseQuery::create($object)
 				->addFilters($filters)
