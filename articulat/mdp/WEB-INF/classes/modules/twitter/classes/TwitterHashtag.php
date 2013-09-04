f4110eb3f02d40a5efbdbefd0ba9d57688a0a512
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-09-04 03:37:46 +0000

    Clases generadas por propel

---
+++ articulat/mdp/WEB-INF/classes/modules/twitter/classes/TwitterHashtag.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+/**
+ * Skeleton subclass for representing a row from the 'twitter_hashtag' table.
+ *
+ * Hashtags de tweets
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.twitter.classes
+ */
+class TwitterHashtag extends BaseTwitterHashtag
+{
+}
