8d9543dc18073ea11fdef4ec625ee11569a3317b
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-09-18 04:10:10 +0000

    *Plugin para twitter

--- externals/lib-phpmvc/smarty3/plugins/modifier.twitterHighlight.php
+++ externals/lib-phpmvc/smarty3/plugins/modifier.twitterHighlight.php
@@ -23,8 +23,15 @@ function smarty_modifier_twitterHighlight($text='')
 	if(strlen($text) > 0) {
 		$tagName = "span";
 		$class = " class='userHash'";
-			$text =  preg_replace('/@+([\w]+)/', "<$tagName$class>".'$0'."</$tagName>", $text);
-			$text =  preg_replace('/(^|\s)#(\w*[a-zA-Z_]+\w*)/', "<$tagName$class><strong>".'$0'."</strong></$tagName>", $text);
+			//usuarios
+			$text =  preg_replace('/^@(\w+)/', "<$tagName$class>".'$0'."</$tagName>", $text);
+			$text =  preg_replace('/@(\w+)/', "<$tagName$class>".'$0'."</$tagName>", $text);
+			//urls
+			$text =  preg_replace('@(https?://([-\w\.]+)+(/([\w/_\.]*(\?\S+)?(#\S+)?)?)?)@', "<$tagName$class>".'$0'."</$tagName>", $text);
+			//hashtags
+			//$text =  preg_replace('/(^|\s)#(\w*[a-zA-Z_]+\w*)/', "<$tagName$class><strong>".'$0'."</strong></$tagName>", $text);
+			//$text =  preg_replace('/#(\w+)/', "<$tagName$class><strong>".'$0'."</strong></$tagName>", $text);
+			$text =  preg_replace('/\#([a-zA-Z0-9_]{1,15}) ?/', "<$tagName$class><strong>".'$0'."</strong></$tagName>", $text);
 	}
 	return($text);
 }
