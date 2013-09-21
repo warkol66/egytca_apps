6363d82cf5c4efe3ca87b1622d9270398db81132
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-09-21 08:25:22 +0000

    *Arreglo para admitir caracteres acentuados en los hashtags

--- externals/lib-phpmvc/smarty3/plugins/modifier.twitterHighlight.php
+++ externals/lib-phpmvc/smarty3/plugins/modifier.twitterHighlight.php
@@ -24,14 +24,14 @@ function smarty_modifier_twitterHighlight($text='')
 		$tagName = "span";
 		$class = " class='userHash'";
 			//usuarios
-			$text =  preg_replace('/^@(\w+)/', "<$tagName$class>".'$0'."</$tagName>", $text);
-			$text =  preg_replace('/@(\w+)/', "<$tagName$class>".'$0'."</$tagName>", $text);
+			$text =  preg_replace('/^@(\w+){1,15}/', "<$tagName$class>".'$0'."</$tagName>", $text);
+			$text =  preg_replace('/@(\w+){1,15}/', "<$tagName$class>".'$0'."</$tagName>", $text);
 			//urls
 			$text =  preg_replace('@(https?://([-\w\.]+)+(/([\w/_\.]*(\?\S+)?(#\S+)?)?)?)@', "<$tagName$class>".'$0'."</$tagName>", $text);
 			//hashtags
 			//$text =  preg_replace('/(^|\s)#(\w*[a-zA-Z_]+\w*)/', "<$tagName$class><strong>".'$0'."</strong></$tagName>", $text);
 			//$text =  preg_replace('/#(\w+)/', "<$tagName$class><strong>".'$0'."</strong></$tagName>", $text);
-			$text =  preg_replace('/\#([a-zA-Z0-9_]{1,15}) ?/', "<$tagName$class><strong>".'$0'."</strong></$tagName>", $text);
+			$text =  preg_replace('/#([^\n\s]+)/', "<$tagName$class><strong>".'$0'."</strong></$tagName>", $text);
 	}
 	return($text);
 }
