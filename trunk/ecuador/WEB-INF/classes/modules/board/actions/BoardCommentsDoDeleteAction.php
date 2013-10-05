2480c9581f2d7336212c71cc0c49283f246dcae3
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-10-05 01:55:54 +0000

    * Agregado de filtros luego del eliminar para redirecionar al listado de donde se eliminó

--- trunk/ecuador/WEB-INF/classes/modules/board/actions/BoardCommentsDoDeleteAction.php
+++ trunk/ecuador/WEB-INF/classes/modules/board/actions/BoardCommentsDoDeleteAction.php
@@ -5,4 +5,15 @@ class BoardCommentsDoDeleteAction extends BaseDoDeleteAction {
 	function __construct() {
 		parent::__construct('BoardComment');
 	}
+
+	protected function postDelete(){
+		parent::postDelete();
+		
+		if(!empty($_GET['filters'])){
+			$filtersUrl = urldecode(http_build_query(array('filters' => $_GET['filters'])));
+			$this->filters = $_GET['filters'];
+			$this->smarty->assign("filtersUrl", $filtersUrl);
+		}
+	}
+
 }
