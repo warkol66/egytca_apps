0c4068ab860b1dd90e1bd8605e1d6be3182c3554
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-10-17 07:55:13 +0000

    *Cambios en la forma de aplicar los filtros a los reportes

--- trunk/articulat/mdp/WEB-INF/classes/modules/twitter/actions/TwitterCampaignsReportFilterXAction.php
+++ trunk/articulat/mdp/WEB-INF/classes/modules/twitter/actions/TwitterCampaignsReportFilterXAction.php
@@ -19,34 +19,29 @@ class TwitterCampaignsReportFilterXAction extends BaseEditAction {
 		$this->smarty->assign("module", $this->module);
 		
 		if(is_object($this->entity)){
+			// obtengo los graficos con los filtros indicados
 			
-			switch($_POST['graph']){
-				case 'value':
-					$value = $_POST['value'];
-					$byValue = TwitterTweetQuery::getAllByValue(null, null, $value);
-					// seteo los valores disponibles para usarlos luego en la creacion del grafico
-					if(array_key_exists('positive',$byValue[0]))
-						$this->smarty->assign('positive', true);
-					if(array_key_exists('neutral',$byValue[0]))
-						$this->smarty->assign('neutral', true);
-					if(array_key_exists('negative',$byValue[0]))
-						$this->smarty->assign('negative', true);
-					$this->smarty->assign('byValue', $byValue);
-				break;
-				case 'relevance':
-					$relevance = $_POST['value'];
-					$byRelevance = TwitterTweetQuery::getAllByRelevance(null, null, $relevance);
-					// seteo los valores disponibles para usarlos luego en la creacion del grafico
-					if(array_key_exists('relevant',$byRelevance[0]))
-						$this->smarty->assign('relevant', true);
-					if(array_key_exists('neutrally_relevant',$byRelevance[0]))
-						$this->smarty->assign('neutrally_relevant', true);
-					if(array_key_exists('irrelevant',$byRelevance[0]))
-						$this->smarty->assign('irrelevant', true);
-					$this->smarty->assign('byRelevance', $byRelevance);
-				break;
+			$value = $_POST['value'];
+			$byValue = TwitterTweetQuery::getAllByValue(null, null, $value);
+			// seteo los valores disponibles para usarlos luego en la creacion del grafico
+			if(array_key_exists('positive',$byValue[0]))
+				$this->smarty->assign('positive', true);
+			if(array_key_exists('neutral',$byValue[0]))
+				$this->smarty->assign('neutral', true);
+			if(array_key_exists('negative',$byValue[0]))
+				$this->smarty->assign('negative', true);
+			$this->smarty->assign('byValue', $byValue);
 				
-			}
+			$relevance = $_POST['relevance'];
+			$byRelevance = TwitterTweetQuery::getAllByRelevance(null, null, $relevance);
+			// seteo los valores disponibles para usarlos luego en la creacion del grafico
+			if(array_key_exists('relevant',$byRelevance[0]))
+				$this->smarty->assign('relevant', true);
+			if(array_key_exists('neutrally_relevant',$byRelevance[0]))
+				$this->smarty->assign('neutrally_relevant', true);
+			if(array_key_exists('irrelevant',$byRelevance[0]))
+				$this->smarty->assign('irrelevant', true);
+			$this->smarty->assign('byRelevance', $byRelevance);
 
 		}
 		
--- trunk/articulat/mdp/WEB-INF/classes/modules/twitter/tpl/TwitterCampaignsReportFilterX.tpl
+++ trunk/articulat/mdp/WEB-INF/classes/modules/twitter/tpl/TwitterCampaignsReportFilterX.tpl
@@ -1,3 +1,18 @@
+	<div id='tweetsByValue'>
+		<h4>Tweets por Valoración</h4>
+		|-assign var=posCount value=count($positive)-|
+		<!--p>Del |-$positive[0]['date']|date_format:'%d/%m/%Y'-| al |-$positive[$posCount - 1]['date']|date_format:'%d/%m/%Y'-|</p-->
+		<div id='byValueMessage'></div>
+		<div id='byValueChart' height='250'></div>
+	</div>
+	
+	<div id='tweetsByRelevance'>
+		<h4>Tweets por Relevancia</h4>
+		|-assign var=posCount value=count($positive)-|
+		<!--p>Del |-$positive[0]['date']|date_format:'%d/%m/%Y'-| al |-$positive[$posCount - 1]['date']|date_format:'%d/%m/%Y'-|</p-->
+		<div id='byRelevanceMessage'></div>
+		<div id='byRelevanceChart'></div>
+	</div>
 <script>
 	|-if !empty($byValue)-|
 	$j('#byValueMessage').html('');
@@ -5,17 +20,16 @@
 	var arrByFilter = [|-foreach from=$byValue item=pos-|{"Fecha":"|-$pos['date']|date_format:'%d-%m-%Y'-|"|-if !empty($positive)-|,"Positivos":"|-$pos['positive']-|"|-/if-||-if !empty($neutral)-|,"Neutros":"|-$pos['neutral']-|"|-/if-||-if !empty($negative)-|,"Negativos":"|-$pos['negative']-|"|-/if-|}|-if !$byValue.last-|,|-/if-||-/foreach-|];
 	
 	barChart(arrByFilter,'byValueChart');
-	
-	|-elseif !empty($byRelevance)-|
+	|-/if-|
+	|-if !empty($byRelevance)-|
 	
 	$j('#byRelevanceMessage').html('');
 
 	var arrByFilter = [|-foreach from=$byRelevance item=pos-|{"Fecha":"|-$pos['date']|date_format:'%d-%m-%Y'-|"|-if !empty($relevant)-|,"Relevantes":"|-$pos['relevant']-|"|-/if-||-if !empty($neutrally_relevant)-|,"Neutros":"|-$pos['neutrally_relevant']-|"|-/if-||-if !empty($irrelevant)-|,"Irrelevantes":"|-$pos['irrelevant']-|"|-/if-|}|-if !$byRelevance.last-|,|-/if-||-/foreach-|];
 	
 	barChart(arrByFilter,'byRelevanceChart');
-	|-else-|
+	|-/if-|
 	$j('#byValueMessage').html('');
 	$j('#byRelevanceMessage').html('');
-	|-/if-|
 
 </script>
