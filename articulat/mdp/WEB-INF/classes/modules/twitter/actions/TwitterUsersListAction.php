ddd08400620693d56e3f81b882c808bd596ccb41
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-10-22 08:54:29 +0000

    *Filtro para obtener candidatos a usuarios

--- articulat/mdp/WEB-INF/classes/modules/twitter/actions/TwitterUsersListAction.php
+++ articulat/mdp/WEB-INF/classes/modules/twitter/actions/TwitterUsersListAction.php
@@ -17,6 +17,13 @@ class TwitterUsersListAction extends BaseListAction {
         if(!empty($_GET['filters']['maxDate'])){
             $this->filters['dateRange']['createdat']['max'] = $_GET['filters']['maxDate'];
 		}
+		
+		/*$asoc = TwitterUserQuery::getCandidateActor(1);
+		echo"<pre>"; print_r($asoc); echo "</pre>";
+		die();*/
+		
+		print_r($this->filters);
+		
 
 	}
 
@@ -24,6 +31,9 @@ class TwitterUsersListAction extends BaseListAction {
 		parent::postList();
 
 		$this->smarty->assign("module", $this->module);
+		
+		echo"<pre>"; print_r($this->results); echo "</pre>";
+		die();
 		
 		$moduleConfig = Common::getModuleConfiguration($this->module);
 		$this->smarty->assign('moduleConfig',$moduleConfig);
