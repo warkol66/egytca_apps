09b9cf7405b0dd5d0d338d09e2211c83c2991bfe
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-11-06 20:28:37 +0000

    *Metodos para el log de twitter

---
+++ articulat/mdp/WEB-INF/classes/modules/twitter/classes/TwitterLog.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+/**
+ * Skeleton subclass for representing a row from the 'twitter_log' table.
+ *
+ * logs de cron de twitter
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.twitter.classes
+ */
+class TwitterLog extends BaseTwitterLog{
+	
+	public static function logTweetSearch($tweetsCount, $ttsCount, $campaignId){
+		$log = new TwitterLog();
+		$log->setTweetscount($tweetsCount);
+		$log->setTrendingTopicscount($ttsCount);
+		$log->setCampaignid($campaignId);
+		$log->save();
+	}
+}
