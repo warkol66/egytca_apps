2f75f6651d72626509b0da6ee3a139945cf29226
gflamerich <gflamerich@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2013-11-12 21:41:12 +0000

    * Agrego los parametros de relevancia al update. * Agrego action para ejecutar con cron para actualizar los usuarios

---
+++ articulat/mdp/WEB-INF/classes/modules/twitter/actions/TwitterUsersUpdateAction.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once 'TwitterConnection.class.php';
+
+class TwitterUsersUpdateAction extends BaseListAction {
+	
+	function __construct() {
+		parent::__construct('TwitterUser');
+	}
+	
+	protected function preList() {
+		parent::preList();
+
+		$this->module = "Twitter";
+		$this->notPaginated = true;
+
+		$max = new DateTime('today');
+		$max->modify('-5 days');
+		$this->filters['updatedAt'] = array('max' => ($max));
+	}
+
+	protected function postList() {
+		parent::postList();
+
+		if ($this->results) {
+			$config = json_decode(file_get_contents(__DIR__.'/../config.json'), true);
+			$twitterConnection = new TwitterConnection($config);
+			$i = 0;
+			foreach ($this->results as $twitterUser) {
+				if ($i == 50)
+					break;
+				$screenn = $twitterUser->getScreenname();
+				if(!empty($screenn))
+					$query = array('screen_name' => $screenn, 'user_id' => $twitterUser->getId());
+				else
+					$query = array('user_id' => $twitterUser->getId());
+
+				if (!empty($query)) {
+					$searchRespone = $twitterConnection->search($query,0,'users');
+					$twitterUser->updateFromTwitter($searchRespone);
+				}
+				$i++;
+			}
+			die;
+		}
+	}
+
+}
--- articulat/mdp/WEB-INF/classes/modules/twitter/actions/TwitterUsersUpdateXAction.php
+++ articulat/mdp/WEB-INF/classes/modules/twitter/actions/TwitterUsersUpdateXAction.php
@@ -3,11 +3,11 @@
 require_once 'TwitterConnection.class.php';
 
 class TwitterUsersUpdateXAction extends BaseEditAction {
-	
+
 	function __construct() {
 		parent::__construct('TwitterUser');
 	}
-	
+
 	protected function preEdit() {
 		parent::preEdit();
 
@@ -17,14 +17,14 @@ class TwitterUsersUpdateXAction extends BaseEditAction {
 
 	protected function postEdit() {
 		parent::postEdit();
-		
+
 		$this->smarty->assign("module", $this->module);
 		$moduleConfig = Common::getModuleConfiguration($this->module);
-		
-		if(is_object($this->entity)){
+
+		if(is_object($this->entity)) {
 			$config = json_decode(file_get_contents(__DIR__.'/../config.json'), true);
 			$twitterConnection = new TwitterConnection($config);
-			
+
 			$screenn = $this->entity->getScreenname();
 			if(!empty($screenn))
 				$query = array('screen_name' => $screenn, 'user_id' => $this->entity->getId());
@@ -32,14 +32,12 @@ class TwitterUsersUpdateXAction extends BaseEditAction {
 				$query = array('user_id' => $this->entity->getId());
 
 			if (!empty($query)) {
-				
 				$searchRespone = $twitterConnection->search($query,0,'users');
-				/*echo"<pre>"; print_r($searchRespone); echo"</pre>";
-				die();*/
 				$this->entity->updateFromTwitter($searchRespone);
 				$this->smarty->assign('twitterUser', $this->entity);
+				$this->smarty->assign('levels',TwitterUser::getInfluenceLevels());
 			}
 		}
-		
+
 	}
 }
