b2aac42e85247ed9dc7bfa949e31bc0a1f7d5e00
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2014-02-24 07:44:31 +0000

    *Clases para descargar los attachments de twitter

---
+++ articulat/mdp/WEB-INF/classes/modules/twitter/classes/AutoDownloaderTwitter.php
@@ -0,0 +1,37 @@
+<?php
+
+class AutoDownloaderTwitter {
+	
+	public function putInQueue($attachment, $mustResample = false) {
+		
+		$url = $attachment->getUrl();
+		$outputFile = $attachment->getRealpath();
+		//return $outputFile;
+		$id = $attachment->getId();
+		
+		if (is_null($outputFile))
+			$outputFile = 'autonamed-'.uniqid();
+		
+		$content = "url=$url\n"."output=$outputFile\n";
+		$content .= "mustResample=" . ($mustResample ? "1" : "0") . "\nid=$id";
+		
+		$queueParentDir = './WEB-INF/classes/modules/twitter/classes/autodwnlder';
+		
+		$queueDir = $queueParentDir . '/' . 'queue';
+		if (!file_exists($queueDir)) {
+			if (Common::isWritable($queueParentDir))
+				mkdir($queueDir, 0777, true);
+			else
+				echo "no uno";
+				throw new Exception("No se puede escribir en el directorio $queueParentDir. Verifique la configuración de permisos.");
+		}
+		
+		if (Common::isWritable($queueDir)) {
+			$filename = $queueDir.'/'.uniqid();
+			file_put_contents($filename, $content);
+		} else {
+			echo "no dos";
+			throw new Exception("No se puede escribir en el directorio $queueDir. Verifique la configuración de permisos.");
+		}
+	}
+}
