a61c8e47fcac4860d78668aaee4012cafdb5afd7
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2014-02-27 23:35:09 +0000

    *Cambio en getLatest tts e inicio de puntaje a tts - todavia no anda

--- trunk/articulat/mdp/WEB-INF/classes/modules/twitter/actions/TwitterCampaignsReportViewAction.php
+++ trunk/articulat/mdp/WEB-INF/classes/modules/twitter/actions/TwitterCampaignsReportViewAction.php
@@ -80,9 +80,9 @@ class TwitterCampaignsReportViewAction extends BaseEditAction {
 				$this->smarty->assign('topUsers', $topUsers);
 				$this->smarty->assign('influentialUsers', $influentialUsers);
 				$this->smarty->assign('tweetsAmount', $tweetsAmount);
-				$this->smarty->assign('trendingTopics', TwitterTrendingTopic::getLatest(10));
-				/*echo"<pre>"; print_r($byValue); echo"</pre>";
-				die();*/
+				$this->smarty->assign('trendingTopics', TwitterTrendingTopicQuery::getLatest(10));
+				echo"<pre>"; print_r(TwitterTrendingTopicQuery::getLatest(10)); echo"</pre>";
+				die();
 				
 				/* Tendencias personalizadas */
 				$treemapInfo = array();
