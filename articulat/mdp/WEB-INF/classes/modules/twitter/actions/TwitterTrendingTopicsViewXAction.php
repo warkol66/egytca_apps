49db3160f16c4244760ad95bd2a310f0ab22cc6d
solange <solange@9ba78dd2-1da9-4e37-ac46-860ebf6affae>
2014-03-05 06:10:14 +0000

    *Seleccion de tts por dia en campaignEdit

---
+++ articulat/mdp/WEB-INF/classes/modules/twitter/actions/TwitterTrendingTopicsViewXAction.php
@@ -0,0 +1,36 @@
+<?php
+
+class TwitterTrendingTopicsViewXAction extends BaseAction {
+
+	function TwitterTrendingTopicsViewXAction() {
+		;
+	}
+	
+	function execute($mapping, $form, &$request, &$response) {
+		
+		parent::execute($mapping, $form, $request, $response);
+		
+		//por ser una action ajax.
+		$this->template->template = "TemplateAjax.tpl";
+
+		$plugInKey = 'SMARTY_PLUGIN';
+		$smarty =& $this->actionServer->getPlugIn($plugInKey);
+		if($smarty == NULL) {
+			echo 'No PlugIn found matching key: '.$plugInKey."<br>\n";
+		}
+		
+		if($_POST['TTview'] == 'next')
+			$date = date('Y-m-d', strtotime($_POST['dateShowing']. ' +1 day'));
+		else
+			$date = date('Y-m-d', strtotime($_POST['dateShowing']. ' -1 day'));
+
+		$timeRange = Common::findFirstAndLastTimes($date);
+		
+		$smarty->assign('dateShowing', $date);
+		$smarty->assign('trendingTopics',TwitterTrendingTopicQuery::getMostTrending($timeRange['from'], $timeRange['to'], 10));
+		return $mapping->findForwardConfig('success');
+
+	}
+}
+
+
--- articulat/mdp/WEB-INF/classes/modules/twitter/tpl/TwitterTrendingTopicsSearchX.tpl
+++ articulat/mdp/WEB-INF/classes/modules/twitter/tpl/TwitterTrendingTopicsSearchX.tpl
@@ -1,5 +1,13 @@
 <script type="text/javascript">
-	$('ttMsgField').innerHTML = '';
+	|-if count($trendingTopics) gt 0-|
+		$('ttMsgField').innerHTML = '';
+	|-else-|
+		$('ttMsgField').innerHTML = '<span class="resultFailure">No se encontraron tweets para esta fecha</span>';
+	|-/if-|
+	|-if isset($dateShowing)-|
+		$('ttDateField').innerHTML = '<span class="resultSuccess">Trending topics del |-$dateShowing|date_format:"%d-%m-%Y"-|</span>';
+		$('dateShowing').setValue('|-$dateShowing-|');
+	|-/if-|
 </script>
 <ul class="twitterTrends">
 	|-foreach from=$trendingTopics item=topic name=for_trendingTopics-|
